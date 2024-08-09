22af3496cd8089e568984d30fec3400ea89c9450
Fenyx Forte <fenyx.studies@gmail.com>
2024-08-09 15:03:36 -0300

    melhorando as transformacoes a serem feitas

--- src/modulos/transformacao/mercado_livre.py
+++ src/modulos/transformacao/mercado_livre.py
@@ -3,52 +3,99 @@ from datetime import datetime
 import polars as pl
 
 
+def tratar_coluna_string(nome_coluna: str) -> pl.Expr:
+    coluna_tratada = (
+        pl.col(nome_coluna)
+        .str.to_uppercase()
+        .str.replace_all(r"[ÁÀÂÃÄÅ]", "A")
+        .str.replace_all(r"[ÉÈÊË]", "E")
+        .str.replace_all(r"[ÍÌÎÏ]", "I")
+        .str.replace_all(r"[ÓÒÔÕÖ]", "O")
+        .str.replace_all(r"[ÚÙÛÜ]", "U")
+        .str.replace_all("Ç", "C", literal=True)
+        .str.replace_all(r"[^A-Z0-9 ]", " ")
+        .str.replace_all(r"\s+", " ")
+        .str.strip_chars()
+    )
+
+    return coluna_tratada
+
+
 def tratar_marca() -> pl.Expr:
-    coluna_tratada = pl.col("marca").fill_null("GENERICA").str.to_uppercase()
+    coluna_tratada = tratar_coluna_string("marca").fill_null("GENERICA")
 
     return coluna_tratada
 
 
 def tratar_produto() -> pl.Expr:
-    coluna_tratada = pl.col("produto").str.to_uppercase()
+    coluna_tratada = tratar_coluna_string("produto").fill_null(
+        "PRODUTO SEM NOME"
+    )
+
+    return coluna_tratada
+
+
+def tratar_preco_velho_reais() -> pl.Expr:
+    coluna_tratada = pl.col("preco_velho_reais").str.replace(
+        ".", "", literal=True
+    )
+
+    return coluna_tratada
+
+
+def tratar_preco_atual_reais() -> pl.Expr:
+    coluna_tratada = pl.col("preco_atual_reais").str.replace(
+        ".", "", literal=True
+    )
 
     return coluna_tratada
 
 
 def tratar_nota_avaliacao() -> pl.Expr:
-    coluna_tratada = pl.col("nota_avaliacao").cast(pl.Float32)
+    coluna_tratada = pl.col("nota_avaliacao").fill_null(0).cast(pl.Float32)
 
     return coluna_tratada
 
 
 def tratar_num_avaliacoes() -> pl.Expr:
     coluna_tratada = (
-        pl.col("num_avaliacoes").str.strip_chars("()").cast(pl.Int32)
+        pl.col("num_avaliacoes")
+        .str.strip_chars("()")
+        .fill_null(0)
+        .cast(pl.Int32)
     )
 
     return coluna_tratada
 
 
-def reduzir_armazenamento_nota_avaliacao() -> pl.Expr:
-    coluna_tratada = pl.col("nota_avaliacao").cast(pl.Float32)
+def tratar_fonte() -> pl.Expr:
+    coluna_tratada = pl.col("_fonte").cast(pl.Categorical)
 
     return coluna_tratada
 
 
-def adiciona_fonte_dados(fonte: str) -> pl.Expr:
-    return pl.lit(fonte).alias("_fonte")
+def tratar_site() -> pl.Expr:
+    coluna_tratada = pl.col("_site").cast(pl.Categorical)
+
+    return coluna_tratada
 
 
-def adiciona_data_coleta(data: datetime) -> pl.Expr:
-    ano = data.year
-    mes = data.month
-    dia = data.day
-    hora = data.hour
-    minuto = data.minute
-    segundo = data.second
-    return pl.datetime(ano, mes, dia, hora, minuto, segundo).alias(
-        "_data_coleta"
-    )
+def tratar_data_coleta() -> pl.Expr:
+    coluna_tratada = pl.col("_data_coleta").str.to_datetime()
+
+    return coluna_tratada
+
+
+def tratar_pagina() -> pl.Expr:
+    coluna_tratada = pl.col("_pagina").cast(pl.Int8)
+
+    return coluna_tratada
+
+
+def tratar_ordem() -> pl.Expr:
+    coluna_tratada = pl.col("_ordem").cast(pl.Int8)
+
+    return coluna_tratada
 
 
 def adiciona_preco_completo(
@@ -56,17 +103,13 @@ def adiciona_preco_completo(
 ) -> pl.Expr:
     coluna_nova = (
         pl.when(
-            (pl.col(col_preco_reais).is_not_null())
-            & (pl.col(col_preco_centavos).is_not_null())
+            pl.col(col_preco_reais).is_not_null()
+            & pl.col(col_preco_centavos).is_not_null()
         )
-        .then(
-            pl.col(col_preco_reais).str.replace(".", "", literal=True)
-            + "."
-            + pl.col(col_preco_centavos)
-        )
+        .then(pl.col(col_preco_reais) + "." + pl.col(col_preco_centavos))
         .otherwise(
             pl.when(pl.col(col_preco_centavos).is_null())
-            .then(pl.col(col_preco_reais).str.replace(".", "", literal=True))
+            .then(pl.col(col_preco_reais))
             .otherwise(None)
         )
         .cast(pl.Float32)
@@ -76,8 +119,36 @@ def adiciona_preco_completo(
     return coluna_nova
 
 
+def adicionar_preco_velho_completo() -> pl.Expr:
+    coluna_nova = adiciona_preco_completo(
+        col_preco_reais="preco_velho_reais",
+        col_preco_centavos="preco_velho_centavos",
+        nome_coluna_nova="preco_velho",
+    )
+
+    return coluna_nova
+
+
+def adicionar_preco_atual_completo() -> pl.Expr:
+    coluna_nova = adiciona_preco_completo(
+        col_preco_reais="preco_atual_reais",
+        col_preco_centavos="preco_atual_centavos",
+        nome_coluna_nova="preco_atual",
+    )
+
+    return coluna_nova
+
+
+def tratar_preco_velho() -> pl.Expr:
+    coluna_tradada = pl.col("preco_velho").fill_null(pl.col("preco_atual"))
+
+    return coluna_tradada
+
+
 def adicionar_promocao() -> pl.Expr:
-    return pl.col("preco_velho").is_null().alias("promocao")
+    coluna_nova = pl.col("preco_velho").is_not_null().alias("promocao")
+
+    return coluna_nova
 
 
 def adicionar_percentual_promocao() -> pl.Expr:
