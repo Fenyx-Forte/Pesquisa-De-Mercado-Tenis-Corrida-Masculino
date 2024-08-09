9315f9bad4ae9a8167ef26d18781994d36bf5b78
Fenyx Forte <fenyx.studies@gmail.com>
2024-08-09 10:28:47 -0300

    melhorando e organizando o webscraping

--- src/webscraping/spiders/mercadolivre.py
+++ src/webscraping/spiders/mercadolivre.py
@@ -1,60 +1,158 @@
 import scrapy
-from modulos.uteis import teste_scrapy
+from modulos.uteis import meu_tempo
+
+
+def seletor_caixa_produtos() -> str:
+    return "div.ui-search-result__content"
+
+
+def seletor_marca() -> str:
+    return "span.ui-search-item__brand-discoverability.ui-search-item__group__element::text"
+
+
+def seletor_nome_produto() -> str:
+    return "h2.ui-search-item__title::text"
+
+
+def seletor_nota_avaliacao() -> str:
+    return "span.ui-search-reviews__rating-number::text"
+
+
+def seletor_num_avaliacoes() -> str:
+    return "span.ui-search-reviews__amount::text"
+
+
+def seletor_caixa_precos() -> str:
+    return "div.ui-search-price.ui-search-price--size-medium"
+
+
+def seletor_caixa_preco_velho() -> str:
+    return "s.ui-search-price__original-value"
+
+
+def seletor_caixa_preco_atual() -> str:
+    return "div.ui-search-price__second-line"
+
+
+def seletor_preco_reais() -> str:
+    return "span.andes-money-amount__fraction::text"
+
+
+def seletor_preco_centavos() -> str:
+    return "span.andes-money-amount__cents::text"
+
+
+def seletor_proxima_pagina() -> str:
+    return "li.andes-pagination__button.andes-pagination__button--next a::attr(href)"
+
+
+def retorna_marca(produto) -> str:
+    marca = produto.css(seletor_marca()).get()
+
+    return marca
+
+
+def retorna_nome_produto(produto) -> str:
+    nome_produto = produto.css(seletor_nome_produto()).get()
+
+    return nome_produto
+
+
+def retorna_nota_avaliacao(produto) -> str:
+    nota_avaliacao = produto.css(seletor_nota_avaliacao()).get()
+
+    return nota_avaliacao
+
+
+def retorna_num_avaliacoes(produto) -> str:
+    num_avaliacoes = produto.css(seletor_num_avaliacoes()).get()
+
+    return num_avaliacoes
+
+
+def retorna_preco_velho_reais(produto) -> str:
+    caixa_precos = produto.css(seletor_caixa_precos()).get()
+
+    caixa_preco_velho = caixa_precos.css(seletor_caixa_preco_velho()).get()
+
+    preco_velho_reais = caixa_preco_velho.css(seletor_preco_reais()).get()
+
+    return preco_velho_reais
+
+
+def retorna_preco_velho_centavos(produto) -> str:
+    caixa_precos = produto.css(seletor_caixa_precos()).get()
+
+    caixa_preco_velho = caixa_precos.css(seletor_caixa_preco_velho()).get()
+
+    preco_velho_centavos = caixa_preco_velho.css(seletor_preco_centavos()).get()
+
+    return preco_velho_centavos
+
+
+def retorna_preco_atual_reais(produto) -> str:
+    caixa_precos = produto.css(seletor_caixa_precos()).get()
+
+    caixa_preco_atual = caixa_precos.css(seletor_caixa_preco_atual()).get()
+
+    preco_atual_reais = caixa_preco_atual.css(seletor_preco_reais()).get()
+
+    return preco_atual_reais
+
+
+def retorna_preco_atual_centavos(produto) -> str:
+    caixa_precos = produto.css(seletor_caixa_precos()).get()
+
+    caixa_preco_atual = caixa_precos.css(seletor_caixa_preco_atual()).get()
+
+    preco_atual_centavos = caixa_preco_atual.css(seletor_preco_centavos()).get()
+
+    return preco_atual_centavos
+
+
+def retorna_proxima_pagina(response) -> str:
+    proxima_pagina = response.css(seletor_proxima_pagina()).get()
+
+    return proxima_pagina
 
 
 class MercadoLivreSpider(scrapy.Spider):
     name = "mercadolivre"
     start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
-    page_count = 1
-    max_pages = 10
+    fonte = "https://lista.mercadolivre.com.br/tenis-corrida-masculino"
+    site = "MERCADO LIVRE"
+    cont_pagina = 1
+    max_pagina = 10
+    ordem_produto = 0
 
     def parse(self, response):
-        produtos_seletor = "div.ui-search-result__content"
+        data_coleta = meu_tempo.data_agora_brasilia()
 
-        produtos = response.css(produtos_seletor)
-
-        marca_seletor = "span.ui-search-item__brand-discoverability.ui-search-item__group__element::text"
-        produto_seletor = "h2.ui-search-item__title::text"
-        nota_avaliacao_seletor = "span.ui-search-reviews__rating-number::text"
-        num_avaliacoes_seletor = "span.ui-search-reviews__amount::text"
-
-        preco_real_seletor = "span.andes-money-amount__fraction::text"
-        preco_centavo_seletor = "span.andes-money-amount__cents::text"
+        produtos = response.css(seletor_caixa_produtos())
 
         for produto in produtos:
-            precos_reais = produto.css(preco_real_seletor).getall()
-            precos_centavos = produto.css(preco_centavo_seletor).getall()
-
-            preco_velho_reais = (
-                precos_reais[0] if len(precos_reais) > 0 else None
-            )
-            preco_atual_reais = (
-                precos_reais[1] if len(precos_reais) > 1 else None
-            )
-
-            preco_velho_centavos = (
-                precos_centavos[0] if len(precos_centavos) > 0 else None
-            )
-            preco_atual_centavos = (
-                precos_centavos[1] if len(precos_centavos) > 1 else None
-            )
+            self.ordem_produtos += 1
 
             yield {
-                "marca": produto.css(marca_seletor).get(),
-                "produto": produto.css(produto_seletor).get(),
-                "preco_velho_reais": preco_velho_reais,
-                "preco_velho_centavos": preco_velho_centavos,
-                "preco_atual_reais": preco_atual_reais,
-                "preco_atual_centavos": preco_atual_centavos,
-                "nota_avaliacao": produto.css(nota_avaliacao_seletor).get(),
-                "num_avaliacoes": produto.css(num_avaliacoes_seletor).get(),
+                "marca": retorna_marca(produto),
+                "produto": retorna_nome_produto(produto),
+                "preco_velho_reais": retorna_preco_velho_reais(produto),
+                "preco_velho_centavos": retorna_preco_velho_centavos(produto),
+                "preco_atual_reais": retorna_preco_atual_reais(),
+                "preco_atual_centavos": retorna_preco_atual_centavos(),
+                "nota_avaliacao": retorna_nota_avaliacao(produto),
+                "num_avaliacoes": retorna_num_avaliacoes(produto),
+                "_fonte": self.fonte,
+                "_site": self.site,
+                "_data_coleta": data_coleta,
+                "_pagina": self.cont_pagina,
+                "_ordem": self.ordem_produto,
             }
 
-        if self.page_count < self.max_pages:
-            next_page = response.css(
-                "li.andes-pagination__button.andes-pagination__button--next a::attr(href)"
-            ).get()
+        if self.cont_pagina < self.max_pagina:
+            proxima_pagina = retorna_proxima_pagina(response)
 
-            if next_page:
-                self.page_count += 1
-                yield scrapy.Request(url=next_page, callback=self.parse)
+            if proxima_pagina:
+                self.cont_pagina += 1
+                self.ordem_produto = 0
+                yield scrapy.Request(url=proxima_pagina, callback=self.parse)
