84f3f4964271c9b3183f872864eef6c90378f4a1
Fenyx Forte <fenyx.studies@gmail.com>
2024-08-14 06:42:04 -0300

    simplificando o uso do modulo

--- src/modulos/uteis/meu_tempo.py
+++ src/modulos/uteis/meu_tempo.py
@@ -5,17 +5,9 @@ def fuso_horario_brasilia() -> datetime.timezone:
     return datetime.timezone(datetime.timedelta(hours=-3), "BRT")
 
 
-def data_agora(timezone: datetime.timezone) -> datetime.datetime:
-    return datetime.datetime.now(timezone)
+def data_agora() -> datetime.datetime:
+    return datetime.datetime.now(fuso_horario_brasilia)
 
 
-def data_agora_string(timezone) -> str:
-    return data_agora(timezone).strftime("%d/%m/%Y %H:%M")
-
-
-def data_agora_brasilia_str() -> str:
-    return data_agora_string(fuso_horario_brasilia())
-
-
-def data_agora_brasilia() -> datetime.datetime:
-    return data_agora(fuso_horario_brasilia())
+def data_agora_string() -> str:
+    return data_agora().strftime("%d/%m/%Y %H:%M")
