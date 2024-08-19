aa52b7581b0c0bf2d72ac2ab6c728cd938bfa7b3
Fenyx Forte <fenyx.studies@gmail.com>
2024-08-19 12:50:21 -0300

    criar tabela mercado_livre

---
+++ sql/ddl/criar_tabela.sql
@@ -0,0 +1,15 @@
+create table if not exists mercado_livre (
+    marca varchar(255) not null
+  , produto varchar(255) not null
+  , preco_velho decimal(7, 2) not null
+  , preco_atual decimal(7, 2) not null
+  , promocao boolean not null
+  , percentual_promocao decimal(5, 2) not null
+  , nota_avaliacao decimal(3, 2) not null
+  , num_avaliacoes integer not null
+  , _fonte varchar(255) not null
+  , _site varchar(255) not null
+  , _data_coleta timestamp not null
+  , _pagina smallint not null
+  , _ordem smallint not null
+);
