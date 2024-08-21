746b46c00d909981c9084f630b337db55700e2ea
Fenyx Forte <fenyx.studies@gmail.com>
2024-08-21 08:18:27 -0300

    otimizando dashboard

--- src/dashboard_app.py
+++ src/dashboard_app.py
@@ -1,4 +1,11 @@
 from dashboard import dash_dash
+from modulos.uteis import carregar_env
+
+
+def ativar_configuracoes():
+    carregar_env.carregar_env()
+
 
 if __name__ == "__main__":
-    dash_dash.dashboard()
+    ativar_configuracoes()
+    dash_dash.meu_dashboard()
