f07aff09f73382619ef804d48469532f91bc3af7
Fenyx Forte <fenyx.studies@gmail.com>
2024-08-24 14:37:17 -0300

    puxar percentual_promocao do banco de dados como valor entre 0 e 1

--- src/modulos/uteis/minhas_queries.py
+++ src/modulos/uteis/minhas_queries.py
@@ -6,7 +6,7 @@ def dados_mais_recentes_do_banco_de_dados() -> str:
         , preco_velho::FLOAT4 AS preco_velho
         , preco_atual::FLOAT4 AS preco_atual
         , promocao
-        , percentual_promocao::FLOAT4 AS percentual_promocao
+        , (percentual_promocao/100)::FLOAT4 AS percentual_promocao
         , nota_avaliacao::FLOAT4 AS nota_avaliacao
         , num_avaliacoes::INT4 AS num_avaliacoes
         , _data_coleta
