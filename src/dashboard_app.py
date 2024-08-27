071abc764bb51c29f3bccfd80d5fa30f825ec7ca
Fenyx Forte <fenyx.studies@gmail.com>
2024-08-27 14:09:15 -0300

    melhor organizacao da aplicacao

--- src/dashboard_app.py
+++ src/dashboard_app.py
@@ -1,117 +1,29 @@
-from dash import Dash, Input, Output, callback, page_container
-from dash.dcc import Location
-from dash_bootstrap_components import Col, Container, Row, Stack, icons, themes
-from dashboard import cabecalho, minha_sidebar, processamento_dados
-from modulos.uteis import carregar_env
+from dash import Dash
+
+from dashboard import configuracoes_dash, processamento_dados
 
 
 def aplicacao():
-    # Inicializacao dados
-    ## Carregando env
-    carregar_env.carregar_env()
+    processamento_dados.inicializacao()
 
-    ## Query banco de dados
-    global df
-    df = processamento_dados.dados_mais_recentes()
+    app = Dash(__name__, **configuracoes_dash.configuracoes_app())
 
-    ## Processamento
-    ### Cabecalho
-    global data_coleta
-    data_coleta = processamento_dados.data_coleta(df)
-
-    ### Pagina 2
-    global pagina_2_dag_dados
-    pagina_2_dag_dados = processamento_dados.inicializacao_pagina_2(df)
-
-    # Dashboard
-    app = Dash(
-        __name__,
-        external_stylesheets=[themes.LUMEN, icons.FONT_AWESOME],
-        update_title=None,
-        assets_folder="../assets/",
-        use_pages=True,
-        pages_folder="./dashboard/paginas",
-        suppress_callback_exceptions=True,
-        meta_tags=[
-            {
-                "name": "viewport",
-                "content": "width=device-width, initial-scale=1.0",
-            },
-            {
-                "http-equiv": "Content-Language",
-                "content": "pt-BR",
-            },
-            {
-                "name": "author",
-                "content": "Fenyx Forte",
-            },
-            {
-                "name": "application-name",
-                "content": "Pesquisa de Mercado: Tênis de Corrida no Mercado Livre",
-            },
-            {
-                "name": "keywords",
-                "content": "webscraping dashboard dados",
-            },
-            {
-                "name": "google",
-                "content": "notranslate",
-            },
-            {
-                "name": "robots",
-                "content": "index, follow",
-            },
-            {
-                "name": "googlebot",
-                "content": "index, follow",
-            },
-        ],
-    )
-
-    app.layout = Container(
-        [
-            Location(id="url"),
-            Row(
-                [
-                    Col(minha_sidebar.sidebar(), width="auto"),
-                    Col(
-                        Stack(
-                            [
-                                cabecalho.cabecalho(data_coleta),
-                                page_container,
-                            ],
-                        ),
-                        width=True,
-                    ),
-                ],
-            ),
-        ],
-        fluid=True,
-    )
+    app.layout = configuracoes_dash.layout_app()
 
     server = app.server
 
     return server
 
 
-# Callbacks
-## Inicializacao
-### Pagina 2
-@callback(
-    Output("meu-dag", "rowData"),
-    Input("input-estatico", "children"),
-)
-def inicializacao_pagina_2(_):
-    return pagina_2_dag_dados
+def desenvolvimento():
+    processamento_dados.inicializacao()
+
+    app = Dash(__name__, **configuracoes_dash.configuracoes_app())
+
+    app.layout = configuracoes_dash.layout_app()
+
+    app.run(debug=True, dev_tools_hot_reload=False)
 
 
-## Pagina 2
-@callback(
-    Output("meu-dag", "exportDataAsCsv"),
-    Input("botao-exportar-csv", "n_clicks"),
-    prevent_initial_call=True,
-)
-def exportar_csv(n_clicks):
-    if n_clicks:
-        return True
-    return False
+if __name__ == "__main__":
+    desenvolvimento()
