8b5113a278595cc3bb540cbdd9a67942af3bc8dc
Fenyx Forte <fenyx.studies@gmail.com>
2024-08-28 10:00:23 -0300

    atualizando configuracoes gunicorn

--- src/dashboard/configuracoes/gunicorn_dev.py
+++ src/dashboard/configuracoes/gunicorn_dev.py
@@ -3,19 +3,22 @@ wsgi_app = "dashboard_app:aplicacao()"
 # The socket to bind
 bind = "127.0.0.1:8051"
 
+# Type Worker
+worker_class = "sync"
+
 # The number of worker processes for handling requests
 workers = 3
 
 # Threads
 threads = 1
 
-# Restart workers when code changes (development only!)
-# reload = True
+# Load application code before the worker processes are forked
+preload_app = False
 
 # Timeout in seconds
 timeout = 30
 
-preload_app = True
-
 # The granularity of Error log outputs
 loglevel = "info"
+
+pidfile = "gunicorn.pid"
--- src/dashboard/configuracoes/gunicorn_prod.py
+++ src/dashboard/configuracoes/gunicorn_prod.py
@@ -6,24 +6,29 @@ wsgi_app = "dashboard_app:aplicacao()"
 bind = "0.0.0.0:8501"
 
 # Type Worker
-worker_class = "gthread"
+worker_class = "sync"
 
 # The number of worker processes for handling requests
 workers = cpu_count() * 2 + 1
 
 # Threads
-threads = cpu_count() * 2 + 1
+threads = 1
 
 # Load application code before the worker processes are forked
-preload_app = True
+preload_app = False
 
 # Timeout in seconds
 timeout = 120
 
-keepalive = 5
+# The number of seconds to wait for requests on a Keep-Alive connection.
+# sync worker does not support persistent connections and will ignore this option.
+# keepalive = 5
 
 # Evitar erro no Docker
 worker_tmp_dir = "/dev/shm"
 
 # The granularity of Error log outputs
 loglevel = "info"
+
+# Ver como melhorar a questao do pidfile
+pidfile = "gunicorn.pid"
