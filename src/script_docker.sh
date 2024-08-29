4eb9961619525ec4648f29da306bc4f51567f64e
Fenyx Forte <fenyx.studies@gmail.com>
2024-08-29 10:57:46 -0300

    corrigindo inicializacao do nginx

--- src/script_docker.sh
+++ src/script_docker.sh
@@ -13,13 +13,16 @@ chown -R www-data:www-data /var/log/nginx
 
 # gunicorn
 mkdir -p /var/log/gunicorn
+mkdir -p /var/run/gunicorn
+touch ./var/run/gunicorn/gunicorn_prod.pid
 touch ./var/log/gunicorn/access.log
 touch ./var/log/gunicorn/error.log
 
+chown -R www-data:www-data /var/run/gunicorn
 chown -R www-data:www-data /var/log/gunicorn
 
 # Iniciar o nginx
-nginx -g 'daemon off;' &
+/usr/sbin/nginx -g 'daemon off;' &
 
 cd src/
 
