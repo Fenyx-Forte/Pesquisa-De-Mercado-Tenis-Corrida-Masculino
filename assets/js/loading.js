184127a04dc8335ee8f68f5005d3ee325a3e28ec
Fenyx Forte <fenyx.studies@gmail.com>
2024-09-02 12:52:07 -0300

    melhorando pagina de loading

---
+++ assets/js/loading.js
@@ -0,0 +1,15 @@
+if (document.getElementById("minha-pagina-carregamento")) {
+    let seconds = 40;
+    const countdownElement = document.getElementById("seconds");
+    const timeUnitElement = document.getElementById("time-unit");
+
+    function updateCountdown() {
+        if (seconds > 0) {
+            seconds--;
+            countdownElement.textContent = seconds;
+            timeUnitElement.textContent = seconds === 1 ? "segundo" : "segundos";
+        }
+    }
+
+    setInterval(updateCountdown, 1000);
+}
