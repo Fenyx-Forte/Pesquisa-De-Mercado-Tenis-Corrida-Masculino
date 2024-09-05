d98c669394976d4fe7fb20a04750da77c1557e44
Fenyx Forte <fenyx.studies@gmail.com>
2024-09-05 14:32:12 -0300

    adicionando metodo para tratar o maximo possivel os produtos que vem sem marca

--- sql/transformacao/tratamento_mercado_livre.sql
+++ sql/transformacao/tratamento_mercado_livre.sql
@@ -1,3 +1,10 @@
+create table
+  marcas as
+select
+  marcas_unicas.marca
+from
+  '../dados/marcas_unicas/marcas_unicas.csv' as marcas_unicas;
+
 create function tratar_string(coluna)
 as trim(
     regexp_replace(
@@ -27,6 +34,7 @@ as cast(
       case
         when col_preco_reais is not null and col_preco_centavos is not null
           then concat(replace(col_preco_reais, '.', ''), '.', col_preco_centavos)
+        -- Se col_preco_reais for NULL, entao eu quero que o resultado seja NULL
         else
           replace(col_preco_reais, '.', '')
       end
@@ -35,53 +43,72 @@ as cast(
 
 with fase_1 as (
   select
-      coalesce(
-        tratar_string(marca), 'GENERICA'
-      ) as marca
-    , coalesce(
-        tratar_string(produto), 'PRODUTO SEM NOME'
-      ) as produto
-    , adicionar_preco_completo(preco_velho_reais, preco_velho_centavos) as preco_velho
-    , adicionar_preco_completo(preco_atual_reais, preco_atual_centavos) as preco_atual
-    , (preco_velho_reais is not null) as promocao
+      coalesce(tratar_string(df.marca), 'GENERICA') as marca
+    , coalesce(tratar_string(df.produto), 'PRODUTO SEM NOME') as produto
+    , adicionar_preco_completo(df.preco_velho_reais, df.preco_velho_centavos) as preco_velho
+    , adicionar_preco_completo(df.preco_atual_reais, df.preco_atual_centavos) as preco_atual
+    , (df.preco_velho_reais is not null) as promocao
     , cast(
-        coalesce(nota_avaliacao, '0') as decimal(4, 2)
+        coalesce(df.nota_avaliacao, '0') as decimal(4, 2)
       ) as nota_avaliacao
     , cast(
-        coalesce(trim(both '()' from num_avaliacoes), '0')
+        coalesce(trim(both '()' from df.num_avaliacoes), '0')
         as uinteger
       ) as num_avaliacoes
-    , _fonte
-    , _site
-    , _data_coleta::date as data_coleta
-    , _data_coleta::time as horario_coleta
-    , cast(_pagina as utinyint) as _pagina
-    , cast(_ordem as utinyint) as _ordem
+    , df._fonte
+    , df._site
+    , df._data_coleta::date as data_coleta
+    , df._data_coleta::time as horario_coleta
+    , cast(df._pagina as utinyint) as _pagina
+    , cast(df._ordem as utinyint) as _ordem
   from
     df
   where
-    preco_atual_reais is not null
+    df.preco_atual_reais is not null
 )
 
 select distinct
-    marca
-  , produto
-  , coalesce(preco_velho, preco_atual, preco_velho) as preco_velho
-  , preco_atual
-  , promocao
+    case
+      when f.marca = 'GENERICA'
+        then coalesce(
+          (
+            select
+              m.marca
+            from
+              marcas as m
+            where
+				      starts_with(f.produto, m.marca || ' ')
+              or contains(f.produto, ' ' || m.marca || ' ')
+				      or ends_with(f.produto, ' ' || m.marca)
+			      order by
+				      length(m.marca) desc
+			      limit
+				      1
+          )
+          , 'GENERICA'
+        )
+      else
+        f.marca
+    end as marca
+  , f.produto
+  , coalesce(f.preco_velho, f.preco_atual) as preco_velho
+  , f.preco_atual
+  , f.promocao
   , cast(
       case
-        when promocao = true then ( (preco_velho - preco_atual) / preco_velho) * 100
-        else 0
+        when f.promocao = true
+          then ( (f.preco_velho - f.preco_atual) / f.preco_velho) * 100
+        else
+          0
       end as decimal(4, 2)
     ) as percentual_promocao
-  , nota_avaliacao
-  , num_avaliacoes
-  , _fonte
-  , _site
-  , data_coleta as _data_coleta
-  , horario_coleta as _horario_coleta
-  , _pagina
-  , _ordem
+  , f.nota_avaliacao
+  , f.num_avaliacoes
+  , f._fonte
+  , f._site
+  , f.data_coleta as _data_coleta
+  , f.horario_coleta as _horario_coleta
+  , f._pagina
+  , f._ordem
 from
-  fase_1;
+  fase_1 as f;
