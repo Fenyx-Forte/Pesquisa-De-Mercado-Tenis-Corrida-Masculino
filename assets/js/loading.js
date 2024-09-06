feee5761249fde674ae5105da9305039608efb2e
Fenyx Forte <fenyx.studies@gmail.com>
2024-09-06 11:59:12 -0300

    melhorando script de carregamento

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,26 +1,27 @@
 if (document.getElementById("minha-pagina-carregamento")) {
     function checkServer() {
-        var xhr = new XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
+        const method = "GET";
+        const url = "/";
 
-        xhr.onreadystatechange = function() {
+        xhr.open(method, url, true);
+
+        xhr.onreadystatechange = () => {
             if (xhr.readyState === XMLHttpRequest.DONE) {
-                // Verifica se o servidor está pronto pela análise do conteúdo, não apenas pelo status
-                if (xhr.status === 200) {
-                    // Verifica se a resposta contém o conteúdo da aplicação e não da página de loading
-                    if (!xhr.responseText.includes("Obrigado pela paciência!")) {
-                        window.location.reload();
-                    } else {
-                        // Se ainda é a página de loading, tenta novamente após 5 segundos
+                const status = xhr.status;
+                if (status === 0 || (status >= 200 && status < 400)) {
+                    const conteudo = xhr.responseText;
+                    if (conteudo.includes("Aguarde alguns instantes")){
                         setTimeout(checkServer, 5000);
+                    } else{
+                        window.location.reload();
                     }
                 } else {
-                    // Se houver erros como 502, 503 ou 504, tenta novamente após 5 segundos
                     setTimeout(checkServer, 5000);
                 }
             }
         };
 
-        xhr.open('GET', '/', true);
         xhr.send();
     }
 
