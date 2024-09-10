6c8504e30b9d8fbe87c3dc2be85ed80bdcccdcf7
Fenyx Forte <fenyx.studies@gmail.com>
2024-09-10 09:45:56 -0300

    organizando melhor pagina 4 dados

--- src/dashboard/paginas/pagina_4_dados.py
+++ src/dashboard/paginas/pagina_4_dados.py
@@ -7,6 +7,7 @@ from dash import (
     register_page,
 )
 from dash_ag_grid import AgGrid
+from dash_bootstrap_components import Button
 
 from dashboard.uteis import formatacoes, processamento_dados, traducoes
 
@@ -20,75 +21,105 @@ register_page(
 )
 
 
+def titulo() -> html.H1:
+    conteudo = html.H1("Página Dados")
+
+    return conteudo
+
+
+def botao_exportar_csv() -> Button:
+    conteudo = Button(
+        "Exportar CSV",
+        outline=True,
+        color="primary",
+        className="me-1",
+        id="botao-exportar-csv",
+    )
+
+    return conteudo
+
+
+def configuracoes_colunas_tabela_dados() -> list[dict]:
+    configuracoes = [
+        {
+            "headerName": "Marca",
+            "field": "marca",
+            "headerTooltip": "Marca tênis",
+            "cellDataType": "text",
+        },
+        {
+            "headerName": "Produto",
+            "field": "produto",
+            "headerTooltip": "Tênis",
+            "cellDataType": "text",
+        },
+        {
+            "headerName": "Preço Atual",
+            "field": "preco_atual",
+            "headerTooltip": "Preço tênis",
+            "cellDataType": "number",
+            "valueFormatter": {
+                "function": f"{formatacoes.dag_format_pt_br()}.format('$,.2f')(params.value)",
+            },
+        },
+        {
+            "headerName": "Promoção?",
+            "field": "promocao",
+            "headerTooltip": "Tênis em Promoção",
+            "cellDataType": "boolean",
+        },
+        {
+            "headerName": "Desconto",
+            "field": "percentual_promocao",
+            "headerTooltip": "Desconto tênis",
+            "cellDataType": "number",
+            "valueFormatter": {
+                "function": f"{formatacoes.dag_format_pt_br()}.format(',.2%')(params.value)",
+            },
+        },
+    ]
+
+    return configuracoes
+
+
+def tabela_dados() -> AgGrid:
+    conteudo = AgGrid(
+        rowData=processamento_dados.retorna_dados_dag(),
+        id="tabela-dados",
+        columnDefs=configuracoes_colunas_tabela_dados(),
+        defaultColDef={
+            "resizable": True,
+            "filter": True,
+            "headerClass": "center-aligned-header",
+            "cellClass": "center-aligned-cell",
+            "wrapHeaderText": True,
+            "autoHeaderHeight": True,
+        },
+        dashGridOptions={
+            "animateRows": False,
+            "suppressColumnMoveAnimation": True,
+            "pagination": True,
+            "tooltipShowDelay": 500,
+            "alwaysMultiSort": True,
+            "localeText": traducoes.dag_locale_pt_br(),
+        },
+        columnSize="responsiveSizeToFit",
+        csvExportParams={
+            "fileName": "tabela_dados.csv",
+        },
+        className="ag-theme-quartz meu-dag",
+    )
+
+    return conteudo
+
+
 layout = html.Div(
     [
-        html.H1("Página Dados"),
-        html.Div("Conteúdo Página Dados"),
+        titulo(),
         html.Br(),
-        html.Button("Exportar CSV", id="botao-exportar-csv", n_clicks=0),
-        AgGrid(
-            rowData=processamento_dados.retorna_dados_dag(),
-            id="meu-dag",
-            columnDefs=[
-                {
-                    "headerName": "Marca",
-                    "field": "marca",
-                    "headerTooltip": "Marca tênis",
-                    "cellDataType": "text",
-                },
-                {
-                    "headerName": "Produto",
-                    "field": "produto",
-                    "headerTooltip": "Tênis",
-                    "cellDataType": "text",
-                },
-                {
-                    "headerName": "Preço Atual",
-                    "field": "preco_atual",
-                    "headerTooltip": "Preço tênis",
-                    "cellDataType": "number",
-                    "valueFormatter": {
-                        "function": f"{formatacoes.dag_format_pt_br()}.format('$,.2f')(params.value)",
-                    },
-                },
-                {
-                    "headerName": "Promoção?",
-                    "field": "promocao",
-                    "headerTooltip": "Tênis em Promoção",
-                    "cellDataType": "boolean",
-                },
-                {
-                    "headerName": "Desconto",
-                    "field": "percentual_promocao",
-                    "headerTooltip": "Desconto tênis",
-                    "cellDataType": "number",
-                    "valueFormatter": {
-                        "function": f"{formatacoes.dag_format_pt_br()}.format(',.2%')(params.value)",
-                    },
-                },
-            ],
-            defaultColDef={
-                "resizable": True,
-                "filter": True,
-                "headerClass": "center-aligned-header",
-                "cellClass": "center-aligned-cell",
-                "wrapHeaderText": True,
-                "autoHeaderHeight": True,
-            },
-            dashGridOptions={
-                "animateRows": False,
-                "suppressColumnMoveAnimation": True,
-                "pagination": True,
-                "tooltipShowDelay": 500,
-                "alwaysMultiSort": True,
-                "localeText": traducoes.dag_locale_pt_br(),
-            },
-            columnSize="responsiveSizeToFit",
-            csvExportParams={
-                "fileName": "ag_grid_test.csv",
-            },
-            className="ag-theme-quartz meu-dag",
-        ),
+        botao_exportar_csv(),
+        html.Br(),
+        tabela_dados(),
     ],
     className="pagina",
 )
@@ -103,7 +134,7 @@ clientside_callback(
         return false;
     }
     """,
-    Output("meu-dag", "exportDataAsCsv"),
+    Output("tabela-dados", "exportDataAsCsv"),
     Input("botao-exportar-csv", "n_clicks"),
     prevent_initial_call=True,
 )
