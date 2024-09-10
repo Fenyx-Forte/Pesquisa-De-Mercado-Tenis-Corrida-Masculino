22dd94d3c9527e431847d84608e6755ecb577900
Fenyx Forte <fenyx.studies@gmail.com>
2024-09-10 09:17:36 -0300

    organizando melhor o cabecalho

--- src/dashboard/componentes/cabecalho.py
+++ src/dashboard/componentes/cabecalho.py
@@ -3,19 +3,31 @@ from dash import html
 from dashboard.uteis import processamento_dados
 
 
+def titulo() -> html.H1:
+    conteudo = html.H1(
+        "Pesquisa de Mercado: Tênis de Corrida no Mercado Livre",
+        className="text-primary text-center fs-3",
+        id="titulo-aplicacao",
+    )
+
+    return conteudo
+
+
+def informacao_data_coleta() -> html.H4:
+    conteudo = html.H4(
+        f"Data Coleta: {processamento_dados.retorna_data_coleta()}",
+        className="text-center",
+        id="data-coleta",
+    )
+
+    return conteudo
+
+
 def cabecalho() -> html.Div:
     conteudo = html.Div(
         [
-            html.H1(
-                "Pesquisa de Mercado: Tênis de Corrida no Mercado Livre",
-                className="text-primary text-center fs-3",
-                id="titulo-aplicacao",
-            ),
-            html.H4(
-                f"Data Coleta: {processamento_dados.retorna_data_coleta()}",
-                className="text-center",
-                id="data-coleta",
-            ),
+            titulo(),
+            informacao_data_coleta(),
             html.Hr(),
         ],
         className="cabecalho",
