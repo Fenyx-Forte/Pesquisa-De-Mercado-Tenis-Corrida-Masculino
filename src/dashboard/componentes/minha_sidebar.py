fc9188c2fdd7f391deeae53b36c0834526d650d1
Fenyx Forte <fenyx.studies@gmail.com>
2024-09-10 09:34:47 -0300

    organizando melhor a sidebar

--- src/dashboard/componentes/minha_sidebar.py
+++ src/dashboard/componentes/minha_sidebar.py
@@ -8,107 +8,139 @@ from dash import (
 from dash_bootstrap_components import Nav, NavLink
 
 
+def botao_toggle() -> html.Div:
+    conteudo = html.Div(
+        html.I(className="fas fa-bars"),
+        className="meu-toggle-navbar",
+        id="meu-toggle-navbar",
+    )
+
+    return conteudo
+
+
+def links_minhas_informacoes() -> html.Div:
+    conteudo = html.Div(
+        [
+            html.A(
+                [
+                    html.Img(
+                        src="/assets/images/portfolio.ico",
+                        className="meu-icone",
+                    ),
+                    html.Label("Portfólio"),
+                ],
+                href="https://fenyx-forte.github.io/",
+                target="_blank",
+                rel="noreferrer",
+            ),
+            html.Br(),
+            html.A(
+                [
+                    html.I(className="fa-brands fa-linkedin-in"),
+                    html.Label("Linkedin"),
+                ],
+                href="https://www.linkedin.com/in/fenyxforte/",
+                target="_blank",
+                rel="noreferrer",
+            ),
+            html.Br(),
+            html.A(
+                [
+                    html.I(className="fa-brands fa-github"),
+                    html.Label("GitHub"),
+                ],
+                href="https://github.com/Fenyx-Forte",
+                target="_blank",
+                rel="noreferrer",
+            ),
+            html.Br(),
+            html.A(
+                [
+                    html.I(className="fa-brands fa-google"),
+                    html.Label("Gmail"),
+                ],
+                href="mailto:fenyxforte@gmail.com",
+                target="_blank",
+                rel="noreferrer",
+            ),
+        ],
+    )
+
+    return conteudo
+
+
+def minhas_informacoes() -> html.Div:
+    conteudo = html.Div(
+        [
+            html.H1("Fenyx Forte", className="text-primary"),
+            html.Hr(),
+            links_minhas_informacoes(),
+        ]
+    )
+
+    return conteudo
+
+
+def links_paginas() -> Nav:
+    conteudo = Nav(
+        [
+            NavLink(
+                html.Div(page["name"]),
+                href=page["path"],
+                active="exact",
+            )
+            for page in page_registry.values()
+            if page["name"] != "Not Found 404"
+        ],
+        vertical=True,
+        pills=True,
+    )
+
+    return conteudo
+
+
+def links_documentacao() -> html.Div:
+    conteudo = html.Div(
+        [
+            html.A(
+                [
+                    html.I(className="fa-solid fa-code"),
+                    html.Label("Repositório"),
+                ],
+                href="https://github.com/Fenyx-Forte/Analise-De-Dados",
+                target="_blank",
+                rel="noreferrer",
+            ),
+            html.Br(),
+            html.A(
+                [
+                    html.I(className="fa-solid fa-book"),
+                    html.Label(
+                        "Documentação",
+                        className="label-documentacao",
+                    ),
+                ],
+                href="https://github.com/Fenyx-Forte",
+                target="_blank",
+                rel="noreferrer",
+            ),
+        ],
+    )
+
+    return conteudo
+
+
 def sidebar() -> html.Div:
     conteudo = html.Div(
         [
-            html.Div(
-                html.I(className="fas fa-bars"),
-                className="meu-toggle-navbar",
-                id="meu-toggle-navbar",
-            ),
+            botao_toggle(),
             html.Div(
                 [
-                    html.H1("Fenyx Forte", className="text-primary"),
+                    minhas_informacoes(),
                     html.Hr(),
-                    html.Div(
-                        [
-                            html.A(
-                                [
-                                    html.Img(
-                                        src="/assets/images/portfolio.ico",
-                                        className="meu-icone",
-                                    ),
-                                    html.Label("Portfólio"),
-                                ],
-                                href="https://fenyx-forte.github.io/",
-                                target="_blank",
-                                rel="noreferrer",
-                            ),
-                            html.Br(),
-                            html.A(
-                                [
-                                    html.I(
-                                        className="fa-brands fa-linkedin-in"
-                                    ),
-                                    html.Label("Linkedin"),
-                                ],
-                                href="https://www.linkedin.com/in/fenyxforte/",
-                                target="_blank",
-                                rel="noreferrer",
-                            ),
-                            html.Br(),
-                            html.A(
-                                [
-                                    html.I(className="fa-brands fa-github"),
-                                    html.Label("GitHub"),
-                                ],
-                                href="https://github.com/Fenyx-Forte",
-                                target="_blank",
-                                rel="noreferrer",
-                            ),
-                            html.Br(),
-                            html.A(
-                                [
-                                    html.I(className="fa-brands fa-google"),
-                                    html.Label("Gmail"),
-                                ],
-                                href="mailto:fenyxforte@gmail.com",
-                                target="_blank",
-                                rel="noreferrer",
-                            ),
-                        ],
-                    ),
+                    links_paginas(),
                     html.Hr(),
-                    Nav(
-                        [
-                            NavLink(
-                                html.Div(page["name"]),
-                                href=page["path"],
-                                active="exact",
-                            )
-                            for page in page_registry.values()
-                            if page["name"] != "Not Found 404"
-                        ],
-                        vertical=True,
-                        pills=True,
-                    ),
-                    html.Hr(),
-                    html.Div(
-                        [
-                            html.A(
-                                [
-                                    html.I(className="fa-solid fa-code"),
-                                    html.Label("Repositório"),
-                                ],
-                                href="https://github.com/Fenyx-Forte/Analise-De-Dados",
-                                target="_blank",
-                                rel="noreferrer",
-                            ),
-                            html.Br(),
-                            html.A(
-                                [
-                                    html.I(className="fa-solid fa-book"),
-                                    html.Label(
-                                        "Documentação",
-                                        className="label-documentacao",
-                                    ),
-                                ],
-                                href="https://github.com/Fenyx-Forte",
-                                target="_blank",
-                                rel="noreferrer",
-                            ),
-                        ],
-                    ),
+                    links_documentacao(),
                 ],
             ),
         ],
