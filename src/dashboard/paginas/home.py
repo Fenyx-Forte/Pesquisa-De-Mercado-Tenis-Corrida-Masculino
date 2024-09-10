fa35ccf9f813db02db1ef1c4c09d14f727443200
Fenyx Forte <fenyx.studies@gmail.com>
2024-09-10 09:53:41 -0300

    organizando melhor pagina home

--- src/dashboard/paginas/home.py
+++ src/dashboard/paginas/home.py
@@ -16,46 +16,74 @@ register_page(
 )
 
 
+def titulo() -> html.H1:
+    conteudo = html.H1("Titulo Pagina")
+
+    return conteudo
+
+
+def seletor_datas() -> dcc.DatePickerRange:
+    conteudo = dcc.DatePickerRange(
+        id="seletor-datas",
+        start_date_placeholder_text="Data Inicial",
+        end_date_placeholder_text="Data Final",
+        display_format="DD/MM/YYYY",
+        min_date_allowed=processamento_dados.retorna_dia_coleta_mais_antiga(),
+        max_date_allowed=processamento_dados.retorna_dia_coleta_mais_recente(),
+        clearable=True,
+        minimum_nights=0,
+        show_outside_days=False,
+    )
+
+    return conteudo
+
+
+def botao_adicionar_periodo() -> Button:
+    conteudo = Button(
+        "Adicionar Período",
+        outline=True,
+        color="primary",
+        className="me-1",
+        id="botao-adicionar-periodo",
+    )
+
+    return conteudo
+
+
+def grafico_top_10_marcas() -> dcc.Graph:
+    figura = px.bar(
+        processamento_dados.retorna_top_10_marcas_dados_mais_recentes(),
+        x="Marca",
+        y="Porcentagem",
+        color="Período",
+        barmode="group",
+    ).update_layout(dragmode=False)
+
+    conteudo = dcc.Graph(
+        figure=figura,
+        config={
+            "displayModeBar": False,
+            "doubleClick": False,
+            "editSelection": False,
+            "editable": False,
+            "scrollZoom": False,
+            "showTips": False,
+        },
+        id="grafico-top-10-marcas",
+    )
+
+    return conteudo
+
+
 layout = html.Div(
     [
-        dcc.DatePickerRange(
-            id="intervalo-datas",
-            start_date_placeholder_text="Data Inicial",
-            end_date_placeholder_text="Data Final",
-            display_format="DD/MM/YYYY",
-            min_date_allowed=processamento_dados.retorna_dia_coleta_mais_antiga(),
-            max_date_allowed=processamento_dados.retorna_dia_coleta_mais_recente(),
-            clearable=True,
-            minimum_nights=0,
-            show_outside_days=False,
-        ),
+        titulo(),
+        html.Br(),
+        seletor_datas(),
         html.Br(),
         html.Br(),
-        Button(
-            "Adicionar Período",
-            outline=True,
-            color="primary",
-            className="me-1",
-            id="botao-adicionar-periodo",
-        ),
-        dcc.Graph(
-            figure=px.bar(
-                processamento_dados.retorna_top_10_marcas_dados_mais_recentes(),
-                x="Marca",
-                y="Porcentagem",
-                color="Período",
-                barmode="group",
-            ).update_layout(dragmode=False),
-            config={
-                "displayModeBar": False,
-                "doubleClick": False,
-                "editSelection": False,
-                "editable": False,
-                "scrollZoom": False,
-                "showTips": False,
-            },
-            id="grafico-top-10-marcas",
-        ),
+        botao_adicionar_periodo(),
+        grafico_top_10_marcas(),
     ],
     className="pagina",
 )
@@ -63,11 +91,11 @@ layout = html.Div(
 
 @callback(
     Output("grafico-top-10-marcas", "figure"),
-    Output("intervalo-datas", "start_date"),
-    Output("intervalo-datas", "end_date"),
+    Output("seletor-datas", "start_date"),
+    Output("seletor-datas", "end_date"),
     Input("botao-adicionar-periodo", "n_clicks"),
-    State("intervalo-datas", "start_date"),
-    State("intervalo-datas", "end_date"),
+    State("seletor-datas", "start_date"),
+    State("seletor-datas", "end_date"),
     State("grafico-top-10-marcas", "figure"),
     prevent_initial_call=True,
 )
@@ -86,7 +114,7 @@ def adicionar_comparacao(n_clicks, data_inicio, data_fim, dados_grafico_atual):
         dados_grafico_atual, data_inicio, data_fim
     )
 
-    figure = px.bar(
+    figura_nova = px.bar(
         dados_grafico,
         x="Marca",
         y="Porcentagem",
@@ -94,4 +122,4 @@ def adicionar_comparacao(n_clicks, data_inicio, data_fim, dados_grafico_atual):
         barmode="group",
     ).update_layout(dragmode=False)
 
-    return figure, None, None
+    return figura_nova, None, None
