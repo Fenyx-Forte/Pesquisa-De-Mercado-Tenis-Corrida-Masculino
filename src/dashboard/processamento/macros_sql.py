eae1d573d06ca0ebc4135f001b0e3e58742d282f
Fenyx Forte <fenyx.studies@gmail.com>
2024-09-16 16:45:30 -0300

    atualizando macro usada no top 10 marcas e adicionando macro da pagina 4

--- src/dashboard/processamento/macros_sql.py
+++ src/dashboard/processamento/macros_sql.py
@@ -21,18 +21,53 @@ def macro_dados_completos_por_periodo() -> str:
 def macro_top_10_marcas_periodo() -> str:
     query = """
     CREATE OR REPLACE MACRO top_10_marcas_periodo(periodo, data_inicio, data_fim) AS TABLE
+        WITH porcentagem_por_dia AS (
+            SELECT
+                marca AS marca
+                , (
+                    COUNT(*) * 100.0 / (SUM(COUNT(*)) OVER(PARTITION BY data_coleta))
+                ) AS porcentagem
+            FROM
+                dados_completos_por_periodo(data_inicio, data_fim)
+            GROUP BY
+                data_coleta
+                , marca
+        )
         SELECT
-            dc.marca AS marca
-            , (
-                COUNT(marca) * 100.0 / (SUM(COUNT(*)) OVER())
-            ) AS porcentagem
+            marca AS marca
+            , AVG(porcentagem) AS porcentagem
             , periodo AS periodo
         FROM
-            dados_completos AS dc
-        WHERE
-            dc.data_coleta BETWEEN data_inicio AND data_fim
+            porcentagem_por_dia
         GROUP BY
-            dc.marca;
+            marca;
+    """
+
+    return query
+
+
+def macro_preco_medio_periodo() -> str:
+    query = """
+    CREATE OR REPLACE MACRO preco_medio_e_num_produtos_periodo(data_inicio, data_fim) AS TABLE
+        WITH preco_medio_por_dia AS (
+            SELECT
+                marca AS marca
+                , AVG(preco_atual) AS preco_medio
+                , COUNT(*) AS num_produtos
+            FROM
+                dados_completos_por_periodo(data_inicio, data_fim)
+            GROUP BY
+                data_coleta
+                , marca
+        )
+        SELECT
+            marca
+            , AVG(preco_medio) AS preco_medio
+            , AVG(num_produtos) AS num_produtos
+        FROM
+            preco_medio_por_dia
+        GROUP BY
+            marca;
     """
 
     return query
