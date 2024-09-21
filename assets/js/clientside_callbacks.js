06c55ee05c3dc98e7ff0971ede122d92570de932
Fenyx Forte <fenyx.studies@gmail.com>
2024-09-21 13:59:02 -0300

    colocando clientside callbacks em um arquivo js

---
+++ assets/js/clientside_callbacks.js
@@ -0,0 +1,57 @@
+window.dash_clientside = Object.assign({}, window.dash_clientside, {
+    clientside: {
+        abrir_e_fechar_sidebar: function(n_clicks) {
+            if (n_clicks % 2 === 1) {
+                return "minha-sidebar-escondida"
+            }
+
+            return "minha-sidebar"
+        },
+
+        verificar_datas: function(n_clicks, data_inicio, data_fim, periodo_hoje, periodo_ja_escolhido, periodo_historico) {
+            if (!data_inicio || !data_fim) {
+                return ["Período Inválido", "Selecione as datas usando o calendário ou escreva as datas no formato DD/MM/YYYY."];
+            }
+
+            function formatar_data(data) {
+                const [ano, mes, dia] = data.split('-');
+                return `${dia}/${mes}/${ano}`;
+            }
+
+            const data_inicio_formatada = formatar_data(data_inicio);
+            const data_fim_formatada = formatar_data(data_fim);
+
+            const periodo = `${data_inicio_formatada} - ${data_fim_formatada}`;
+
+            if (periodo === periodo_hoje || periodo === periodo_ja_escolhido || periodo === periodo_historico) {
+                return ["Período Já Adicionado", "Esse período já foi adicionado. Adicione um período diferente."];
+            }
+
+            return ["", ""];
+        },
+
+        abrir_modal: function(titulo) {
+            if (titulo === "") {
+                return window.dash_clientside.no_update;
+            }
+            return true;
+        },
+
+        ranking_direto: function(str1, str2, str3) {
+            let numbers = [parseFloat(str1), parseFloat(str2), parseFloat(str3)];
+
+            let sortedUnique = [...new Set(numbers.slice().sort((a, b) => b - a))];
+
+            return numbers.map(num => `ranking_top_${sortedUnique.indexOf(num) + 1}`);
+        },
+
+        ranking_inverso: function(str1, str2, str3) {
+            let numbers = [parseFloat(str1), parseFloat(str2), parseFloat(str3)];
+
+            let sortedUnique = [...new Set(numbers.slice().sort((a, b) => a - b))];
+
+            return numbers.map(num => `ranking_top_${sortedUnique.indexOf(num) + 1}`);
+        }
+    }
+}
+);
--- src/dashboard/uteis/uteis_processamento.py
+++ src/dashboard/uteis/uteis_processamento.py
@@ -1,70 +1,39 @@
-def callback_verificar_datas() -> str:
-    funcao = """
-    function verificar_datas(n_clicks, data_inicio, data_fim, periodo_hoje, periodo_ja_escolhido, periodo_historico) {
-        if (!data_inicio || !data_fim) {
-            return ["Período Inválido", "Selecione as datas usando o calendário ou escreva as datas no formato DD/MM/YYYY."];
-        }
-
-        function formatar_data(data) {
-            const [ano, mes, dia] = data.split('-');
-            return `${dia}/${mes}/${ano}`;
-        }
-
-        const data_inicio_formatada = formatar_data(data_inicio);
-        const data_fim_formatada = formatar_data(data_fim);
-
-        const periodo = `${data_inicio_formatada} - ${data_fim_formatada}`;
-
-        if (periodo === periodo_hoje || periodo === periodo_ja_escolhido || periodo === periodo_historico) {
-            return ["Período Já Adicionado", "Esse período já foi adicionado. Adicione um período diferente."];
-        }
-
-        return ["", ""];
-    }
-    """
-
-    return funcao
+from dash import ClientsideFunction
 
 
-def callback_abrir_modal() -> str:
-    funcao = """
-    function abrirModal(titulo) {
-        if (titulo === "") {
-            return window.dash_clientside.no_update;
-        }
-        return true;
-    }
-    """
-
-    return funcao
+def callback_abrir_e_fechar_sidebar() -> ClientsideFunction:
+    return ClientsideFunction(
+        namespace="clientside",
+        function_name="abrir_e_fechar_sidebar",
+    )
 
 
-def callback_ranking_direto_valores() -> str:
-    funcao = """
-    function ranking_direto(str1, str2, str3) {
-        let numbers = [parseFloat(str1), parseFloat(str2), parseFloat(str3)];
-
-        let sortedUnique = [...new Set(numbers.slice().sort((a, b) => b - a))];
-
-        return numbers.map(num => `ranking_top_${sortedUnique.indexOf(num) + 1}`);
-    }
-    """
-
-    return funcao
+def callback_verificar_datas() -> ClientsideFunction:
+    return ClientsideFunction(
+        namespace="clientside",
+        function_name="verificar_datas",
+    )
 
 
-def callback_ranking_inverso_valores() -> str:
-    funcao = """
-    function ranking_inverso(str1, str2, str3) {
-        let numbers = [parseFloat(str1), parseFloat(str2), parseFloat(str3)];
+def callback_abrir_modal() -> ClientsideFunction:
+    return ClientsideFunction(
+        namespace="clientside",
+        function_name="abrir_modal",
+    )
 
-        let sortedUnique = [...new Set(numbers.slice().sort((a, b) => a - b))];
 
-        return numbers.map(num => `ranking_top_${sortedUnique.indexOf(num) + 1}`);
-    }
-    """
+def callback_ranking_direto_valores() -> ClientsideFunction:
+    return ClientsideFunction(
+        namespace="clientside",
+        function_name="ranking_direto",
+    )
 
-    return funcao
+
+def callback_ranking_inverso_valores() -> ClientsideFunction:
+    return ClientsideFunction(
+        namespace="clientside",
+        function_name="ranking_inverso",
+    )
 
 
 def formatar_data_pt_br(data: str) -> str:
