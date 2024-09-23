0e4d5aeafabe9f90e296c30629351b40ae8e8334
Fenyx Forte <fenyx.studies@gmail.com>
2024-09-23 12:57:24 -0300

    excluindo arquivo dash_change_title.js

--- assets/dash_js/dash_change_title.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var clientside = window.dash_clientside = window.dash_clientside || {};
-    var ns = clientside["_dashprivate_clientside_funcs"] = clientside["_dashprivate_clientside_funcs"] || {};
-
-    ns["setPageTitle"] = function(data) {
-        document.title = data.title;
-    };
-})();
