cd9862bdc660ef1502fe38059e20861b6239c1c9
Fenyx Forte <fenyx.studies@gmail.com>
2024-10-08 09:11:43 -0300

    corrigindo macro query faixa preco

--- src/dashboard/processamento/macros_sql.py
+++ src/dashboard/processamento/macros_sql.py
@@ -23,15 +23,15 @@ def macro_top_10_marcas_periodo() -> str:
     CREATE OR REPLACE MACRO top_10_marcas_periodo(periodo, data_inicio, data_fim) AS TABLE
         WITH porcentagem_por_dia AS (
             SELECT
-                dcr.marca AS marca
+                dcp.marca AS marca
                 , (
-                    COUNT(*) * 100.0 / (SUM(COUNT(*)) OVER(PARTITION BY dcr.data_coleta))
+                    COUNT(*) * 100.0 / (SUM(COUNT(*)) OVER(PARTITION BY dcp.data_coleta))
                 ) AS porcentagem
             FROM
-                dados_completos_por_periodo(data_inicio, data_fim) AS dcr
+                dados_completos_por_periodo(data_inicio, data_fim) AS dcp
             GROUP BY
-                dcr.data_coleta
-                , dcr.marca
+                dcp.data_coleta
+                , dcp.marca
         )
         SELECT
             ppd.marca AS marca
@@ -51,14 +51,14 @@ def macro_preco_medio_periodo() -> str:
     CREATE OR REPLACE MACRO preco_medio_periodo(data_inicio, data_fim) AS TABLE
         WITH preco_medio_por_dia AS (
             SELECT
-                dcr.marca AS marca
-                , AVG(dcr.preco_atual) AS preco_medio
+                dcp.marca AS marca
+                , AVG(dcp.preco_atual) AS preco_medio
                 , COUNT(*) AS num_produtos
             FROM
-                dados_completos_por_periodo(data_inicio, data_fim) AS dcr
+                dados_completos_por_periodo(data_inicio, data_fim) AS dcp
             GROUP BY
-                dcr.data_coleta
-                , dcr.marca
+                dcp.data_coleta
+                , dcp.marca
         )
         SELECT
             pmd.marca
@@ -76,31 +76,19 @@ def macro_preco_medio_periodo() -> str:
 def macro_faixa_preco_periodo() -> str:
     query = """
     CREATE OR REPLACE MACRO faixa_preco_periodo(data_inicio, data_fim) AS TABLE
-        WITH faixa_preco_por_dia AS (
-            SELECT
-                dcr.marca AS marca
-                , CASE
-                    WHEN dcr.preco_atual < 200 THEN '200'
-                    WHEN dcr.preco_atual BETWEEN 200 AND 400 THEN '200_400'
-                    ELSE '400'
-                  END AS faixa_preco
-                , COUNT(*) AS num_produtos
-            FROM
-                dados_completos_por_periodo(data_inicio, data_fim) AS dcr
-            GROUP BY
-                dcr.data_coleta
-                , dcr.marca
-                , faixa_preco
-        )
         SELECT
-            fpd.marca
-            , fpd.faixa_preco
-            , AVG(fpd.num_produtos) AS num_produtos
+            dcp.marca AS marca
+            , CASE
+                WHEN dcp.preco_atual < 200 THEN '200'
+                WHEN dcp.preco_atual BETWEEN 200 AND 400 THEN '200_400'
+                ELSE '400'
+              END AS faixa_preco
+            , COUNT(*) / COUNT(DISTINCT dcp.data_coleta) AS num_produtos
         FROM
-            faixa_preco_por_dia AS fpd
+            dados_completos_por_periodo(data_inicio, data_fim) AS dcp
         GROUP BY
-            fpd.marca
-            , fpd.faixa_preco;
+            dcp.marca
+            , faixa_preco
     """
 
     return query
@@ -111,16 +99,16 @@ def macro_media_avaliacoes_periodo() -> str:
     CREATE OR REPLACE MACRO media_avaliacoes_periodo(data_inicio, data_fim) AS TABLE
         WITH media_avaliacoes_por_dia AS (
             SELECT
-                dcr.marca AS marca
-                , AVG(dcr.nota_avaliacao) AS media_avaliacao
-                , AVG(dcr.num_avaliacoes) AS media_num_avaliacoes
+                dcp.marca AS marca
+                , AVG(dcp.nota_avaliacao) AS media_avaliacao
+                , AVG(dcp.num_avaliacoes) AS media_num_avaliacoes
             FROM
-                dados_completos_por_periodo(data_inicio, data_fim) AS dcr
+                dados_completos_por_periodo(data_inicio, data_fim) AS dcp
             WHERE
-                dcr.num_avaliacoes > 0
+                dcp.num_avaliacoes > 0
             GROUP BY
-                dcr.data_coleta
-                , dcr.marca
+                dcp.data_coleta
+                , dcp.marca
         )
         SELECT
             mad.marca
@@ -140,16 +128,16 @@ def macro_media_promocoes_periodo() -> str:
     CREATE OR REPLACE MACRO media_promocoes_periodo(data_inicio, data_fim) AS TABLE
         WITH media_promocoes_por_dia AS (
             SELECT
-                dcr.marca AS marca
+                dcp.marca AS marca
                 , COUNT(*) as numero_produtos_em_promocao
-                , AVG(dcr.percentual_promocao) AS percentual_medio_desconto
+                , AVG(dcp.percentual_promocao) AS percentual_medio_desconto
             FROM
-                dados_completos_por_periodo(data_inicio, data_fim) AS dcr
+                dados_completos_por_periodo(data_inicio, data_fim) AS dcp
             WHERE
-                dcr.promocao = true
+                dcp.promocao = true
             GROUP BY
-                dcr.data_coleta
-                , dcr.marca
+                dcp.data_coleta
+                , dcp.marca
         )
         SELECT
             m.marca
