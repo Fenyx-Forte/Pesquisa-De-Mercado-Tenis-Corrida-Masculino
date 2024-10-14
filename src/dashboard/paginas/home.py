b3a208c2b50425e8fd518dacf4f4678084ec485c
Fenyx Forte <fenyx.studies@gmail.com>
2024-10-14 17:39:21 -0300

    inicio estruturacao pagina home

--- src/dashboard/paginas/home.py
+++ src/dashboard/paginas/home.py
@@ -1,4 +1,10 @@
 from dash import html, register_page
+from dash_bootstrap_components import (
+    Col,
+    Row,
+)
+
+from dashboard.uteis import componentes_pagina
 
 register_page(
     __name__,
@@ -10,17 +16,40 @@ register_page(
 )
 
 
-def titulo() -> html.H1:
-    conteudo = html.H1("Página Home")
+def id_pagina() -> str:
+    return "home"
+
+
+def titulo_pagina() -> str:
+    return "Home"
+
+
+def coluna() -> html.Div:
+    conteudo = html.Div("Conteúdo Home")
+
+    return conteudo
+
+
+def colunas() -> Row:
+    conteudo = Row(
+        [
+            Col(
+                coluna(),
+                xs=12,
+                class_name="coluna_hoje",
+            ),
+        ],
+        class_name="linha_colunas",
+    )
 
     return conteudo
 
 
 layout = html.Div(
     [
-        titulo(),
+        componentes_pagina.div_titulo(titulo_pagina()),
         html.Div("Conteúdo Página Home"),
     ],
     className="pagina",
-    id="home",
+    id=id_pagina(),
 )
