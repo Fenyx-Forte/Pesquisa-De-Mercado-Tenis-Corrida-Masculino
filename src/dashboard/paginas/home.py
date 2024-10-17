c993837cc5a8693c5baacef3b7ef5222ba0ca2c3
Fenyx Forte <fenyx.studies@gmail.com>
2024-10-17 18:25:09 -0300

    atualizando pagina home

--- src/dashboard/paginas/home.py
+++ src/dashboard/paginas/home.py
@@ -1,9 +1,12 @@
+from operator import ge
+
 from dash import html, register_page
 from dash_bootstrap_components import (
     Col,
     Row,
 )
 
+from dashboard.processamento import gerenciador
 from dashboard.uteis import componentes_pagina
 
 register_page(
@@ -24,8 +27,330 @@ def titulo_pagina() -> str:
     return "Home"
 
 
+def secao_contexto() -> html.Div:
+    periodo_coleta = gerenciador.retorna_periodo_historico()
+
+    texto_contexto = """
+        Os dados do dashboard são refentes ao resultado de pesquisar por
+        "tenis-corrida-masculino" no site do Mercado Livre. Mais precisamente,
+        são os dados relativos às 10 primeiras páginas dessa pesquisa.
+    """
+
+    conteudo = html.Div(
+        [
+            html.H4("Contexto", className="titulo_um"),
+            html.Div(texto_contexto),
+            html.Div(
+                f"Os dados foram coletados no período de {periodo_coleta}."
+            ),
+            html.Div(
+                "Os dados foram coletados diariamente por volta de 01:00 ou 02:30 da madrugada."
+            ),
+        ],
+        className="secao",
+    )
+
+    return conteudo
+
+
+def secao_foco() -> html.Div:
+    conteudo = html.Div(
+        [
+            html.H4("Foco"),
+            html.Div(
+                "O foco do dashboard é auxiliar a responder as seguintes perguntas:"
+            ),
+            html.Div(
+                "1) Hoje é um dia bom para comprar?",
+                className="perguntas",
+            ),
+            html.Div(
+                "2) Como o dia de hoje se compara a outros dias ou a outros períodos?",
+                className="perguntas",
+            ),
+            html.Div(
+                "3) Quais são as marcas mais procuradas?",
+                className="perguntas",
+            ),
+            html.Div(
+                "4) Quais marcas têm preço médio inferior a R$200?",
+                className="perguntas",
+            ),
+            html.Div(
+                "5) Quais marcas têm preço médio entre R$200 e R$400?",
+                className="perguntas",
+            ),
+            html.Div(
+                "6) Quais marcas têm preço médio acima de R$400?",
+                className="perguntas",
+            ),
+            html.Div(
+                "7) Quais marcas são as mais bem avaliadas?",
+                className="perguntas",
+            ),
+            html.Div(
+                "8) Quais marcas têm o maior número de avaliações?",
+                className="perguntas",
+            ),
+            html.Div(
+                "9) Quais marcas oferecem mais promoções?",
+                className="perguntas",
+            ),
+            html.Div(
+                "10) Quais marcas oferecem os melhores descontos?",
+                className="perguntas",
+            ),
+        ],
+        className="secao",
+    )
+
+    return conteudo
+
+
+def secao_links() -> html.Div:
+    conteudo = html.Div(
+        [
+            html.H4("Links"),
+            html.Div(
+                "(Para abrir o menu lateral, vá para o topo da página. Depois, clique no botão que está do lado esquerdo.)"
+            ),
+            html.Div(
+                "Todos os links do dashboard se encontram no menu lateral."
+            ),
+            html.Div("Minhas informações:", className="subsecao_links"),
+            html.Div(
+                [
+                    html.A(
+                        "Portfólio",
+                        href="https://fenyx-forte.github.io/",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Meu portfólio.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "LinkedIn",
+                        href="https://www.linkedin.com/in/fenyxforte/",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Meu LinkedIn.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "GitHub",
+                        href="https://github.com/Fenyx-Forte",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Meu GitHub.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "Gmail",
+                        href="mailto:fenyxforte@gmail.com",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Meu email.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                "Páginas do dashboard:",
+                className="subsecao_links",
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "Home",
+                        href="/",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Página inicial.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "KPI's",
+                        href="/kpis",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        'Principais indicadores para a pergunta "Hoje é um dia bom para comprar?".',
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "Top 10 Marcas Atuais",
+                        href="/top-10-marcas-atuais",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Como as 10 marcas mais procuradas de hoje estavam em outros períodos?",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "Top 10 Marcas Período",
+                        href="/top-10-marcas-periodo",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Quais são as 10 marcas mais procuradas de cada período?",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "Preço Médio",
+                        href="/preco-medio",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Preço médio por marca.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "Faixa Preço",
+                        href="/faixa-preco",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Faixa de preço por marca.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "Satisfação",
+                        href="/satisfacao",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Satisfação por marca.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "Promoções",
+                        href="/promocoes",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Promoções por marca.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                "Documentação:",
+                className="subsecao_links",
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "Repositório",
+                        href="https://github.com/Fenyx-Forte/Pesquisa-De-Mercado-Tenis-Corrida-Mercado-Livre",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Repositório do projeto.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.A(
+                        "Documentação",
+                        href="https://github.com/Fenyx-Forte/Pesquisa-De-Mercado-Tenis-Corrida-Mercado-Livre",
+                        target="_blank",
+                        rel="noreferrer",
+                        className="links_na_sidebar",
+                    ),
+                    html.Div(
+                        "Documentação do projeto.",
+                        className="resumo_link",
+                    ),
+                ]
+            ),
+        ],
+        className="secao",
+    )
+
+    return conteudo
+
+
 def coluna() -> html.Div:
-    conteudo = html.Div("Conteúdo Home")
+    conteudo = html.Div(
+        [
+            secao_contexto(),
+            secao_foco(),
+            secao_links(),
+        ]
+    )
 
     return conteudo
 
@@ -36,10 +361,9 @@ def colunas() -> Row:
             Col(
                 coluna(),
                 xs=12,
-                class_name="coluna_hoje",
+                class_name="coluna_home",
             ),
         ],
-        class_name="linha_colunas",
     )
 
     return conteudo
@@ -48,7 +372,7 @@ def colunas() -> Row:
 layout = html.Div(
     [
         componentes_pagina.div_titulo(titulo_pagina()),
-        html.Div("Conteúdo Página Home"),
+        colunas(),
     ],
     className="pagina",
     id=id_pagina(),
