a4efa962420faf5baac8aa9d81badff020df7909
Fenyx Forte <fenyx.studies@gmail.com>
2024-12-30 17:56:02 -0300

    docs: documentando modulo

--- src/modulos/uteis/configuracao_loguru.py
+++ src/modulos/uteis/configuracao_loguru.py
@@ -1,41 +1,46 @@
+"""Módulo de configuração do loguru."""
+
 import sys
 
 from loguru import logger
 
 
 def configuracao_loguru() -> None:
-    configuracao = {
-        "handlers": [
-            {
-                "sink": sys.stderr,
-                "level": "INFO",
-                "format": (
-                    "<green>{time:DD-MM-YYYY HH:mm:ss}</green> | "
-                    "<level>{level}</level> | "
-                    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
-                    "{message}"
-                ),
-                "backtrace": False,
-                "diagnose": False,
-                "enqueue": True,
-            },
-            {
-                "sink": "../logs/info.log",
-                "rotation": "1 MB",
-                "delay": True,
-                "level": "INFO",
-                "format": (
-                    "<green>{time:DD-MM-YYYY HH:mm:ss}</green> | "
-                    "<level>{level}</level> | "
-                    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
-                    "{message}"
-                ),
-                "backtrace": False,
-                "diagnose": False,
-                "enqueue": True,
-            },
-        ],
-        "extra": {"user": "someone"},
-    }
+    """Carrega a configuração do loguru."""
+    handlers = [
+        {
+            "sink": sys.stderr,
+            "level": "INFO",
+            "format": (
+                "<green>{time:DD-MM-YYYY HH:mm:ss}</green> | "
+                "<level>{level}</level> | "
+                "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+                "{message}"
+            ),
+            "backtrace": False,
+            "diagnose": False,
+            "enqueue": True,
+        },
+        {
+            "sink": "../logs/info.log",
+            "rotation": "1 MB",
+            "delay": True,
+            "level": "INFO",
+            "format": (
+                "<green>{time:DD-MM-YYYY HH:mm:ss}</green> | "
+                "<level>{level}</level> | "
+                "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+                "{message}"
+            ),
+            "backtrace": False,
+            "diagnose": False,
+            "enqueue": True,
+        },
+    ]
 
-    logger.configure(**configuracao)
+    extra = {"user": "someone"}
+
+    logger.configure(
+        handlers=handlers,
+        extra=extra,
+    )
