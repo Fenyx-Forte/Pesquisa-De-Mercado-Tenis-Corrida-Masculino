5bf0aed652cec1336824706733dd27b36e074028
Fenyx Forte <fenyx.studies@gmail.com>
2024-12-30 17:56:22 -0300

    docs: documentando modulo

--- src/modulos/uteis/configuracao_polars.py
+++ src/modulos/uteis/configuracao_polars.py
@@ -1,5 +1,8 @@
+"""Módulo de configuração do polars."""
+
 import polars as pl
 
 
 def configuracao_polars(caminho_arquivo: str) -> None:
+    """Carrega a configuração do polars."""
     pl.Config.load_from_file(caminho_arquivo)
