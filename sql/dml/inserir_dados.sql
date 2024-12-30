058a73e008384e4d7ee6a0e24173ee9144084579
Fenyx Forte <fenyx.studies@gmail.com>
2024-12-30 15:32:07 -0300

    style: melhorando formatacao sql

--- sql/dml/inserir_dados.sql
+++ sql/dml/inserir_dados.sql
@@ -1,18 +1,30 @@
-insert into db.tenis_corrida (
-    marca
-  , produto
-  , preco_velho
-  , preco_atual
-  , promocao
-  , percentual_promocao
-  , nota_avaliacao
-  , num_avaliacoes
-  , _data_coleta
-  , _horario_coleta
-  , _pagina
-  , _ordem
+insert into
+db.tenis_corrida (
+    marca,
+    produto,
+    preco_velho,
+    preco_atual,
+    promocao,
+    percentual_promocao,
+    nota_avaliacao,
+    num_avaliacoes,
+    _data_coleta,
+    _horario_coleta,
+    _pagina,
+    _ordem
 )
 select
-  *
+    marca,
+    produto,
+    preco_velho,
+    preco_atual,
+    promocao,
+    percentual_promocao,
+    nota_avaliacao,
+    num_avaliacoes,
+    _data_coleta,
+    _horario_coleta,
+    _pagina,
+    _ordem
 from
-  df;
+    df;
