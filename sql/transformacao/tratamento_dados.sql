497020798b7a15f3f4181449bbeac60659dedb6c
Fenyx Forte <fenyx.studies@gmail.com>
2024-12-30 17:51:44 -0300

    fix: melhorando configuracao sqlfluff

--- sql/transformacao/tratamento_dados.sql
+++ sql/transformacao/tratamento_dados.sql
@@ -1,64 +1,95 @@
-create table
-  marcas as
-select
-  marcas_unicas.marca
+create table marcas as
+select marcas_unicas.marca
 from
   '../dados/marcas_unicas/marcas_unicas.csv' as marcas_unicas;
 
-create function tratar_string(coluna)
-as trim(
+create function tratar_string(coluna) as trim(
+  regexp_replace(
     regexp_replace(
-      regexp_replace(
-        replace(
+      replace(
+        regexp_replace(
           regexp_replace(
             regexp_replace(
               regexp_replace(
                 regexp_replace(
-                  regexp_replace(
-                    upper(coluna), '[ÁÀÂÃÄÅ]', 'A', 'g'
-                  ), '[ÉÈÊË]', 'E', 'g'
-                ), '[ÍÌÎÏ]', 'I', 'g'
-              ), '[ÓÒÔÕÖ]', 'O', 'g'
-            ), '[ÚÙÛÜ]', 'U', 'g'
-          ), 'Ç', 'C'
-        ), '[^A-Z0-9 ]', ' ', 'g'
-      ), '\s+', ' ', 'g'
-    )
-  );
+                  upper(coluna), '[ÁÀÂÃÄÅ]', 'A', 'g'
+                ),
+                '[ÉÈÊË]',
+                'E',
+                'g'
+              ),
+              '[ÍÌÎÏ]',
+              'I',
+              'g'
+            ),
+            '[ÓÒÔÕÖ]',
+            'O',
+            'g'
+          ),
+          '[ÚÙÛÜ]',
+          'U',
+          'g'
+        ),
+        'Ç',
+        'C'
+      ),
+      '[^A-Z0-9 ]',
+      ' ',
+      'g'
+    ),
+    '\s+',
+    ' ',
+    'g'
+  )
+);
 
 create function adicionar_preco_completo(
-  col_preco_reais,
-  col_preco_centavos)
-as cast(
-    (
-      case
-        when col_preco_reais is not null and col_preco_centavos is not null
-          then concat(replace(col_preco_reais, '.', ''), '.', col_preco_centavos)
-        -- Se col_preco_reais for NULL, entao eu quero que o resultado seja NULL
-        else
-          replace(col_preco_reais, '.', '')
-      end
-    ) as decimal(9, 2)
-  );
+  col_preco_reais, col_preco_centavos
+) as cast(
+  (
+    case
+      when
+        col_preco_reais is not null
+        and col_preco_centavos is not null
+        then concat(
+          replace(col_preco_reais, '.', ''),
+          '.',
+          col_preco_centavos
+        )
+      -- Se col_preco_reais for NULL, entao eu quero que o resultado seja NULL
+      else replace(col_preco_reais, '.', '')
+    end
+  ) as decimal(9, 2)
+);
 
-with fase_1 as (
+with
+fase_1 as (
   select
-      coalesce(tratar_string(df.marca), 'GENERICA') as marca
-    , coalesce(tratar_string(df.produto), 'PRODUTO SEM NOME') as produto
-    , adicionar_preco_completo(df.preco_velho_reais, df.preco_velho_centavos) as preco_velho
-    , adicionar_preco_completo(df.preco_atual_reais, df.preco_atual_centavos) as preco_atual
-    , (df.preco_velho_reais is not null) as promocao
-    , cast(
-        coalesce(df.nota_avaliacao, '0') as decimal(4, 2)
-      ) as nota_avaliacao
-    , cast(
-        coalesce(trim(both '()' from df.num_avaliacoes), '0')
-        as uinteger
-      ) as num_avaliacoes
-    , df._data_coleta::date as data_coleta
-    , df._data_coleta::time as horario_coleta
-    , cast(df._pagina as utinyint) as _pagina
-    , cast(df._ordem as utinyint) as _ordem
+    cast(coalesce(df.nota_avaliacao, '0') as decimal(4, 2))
+      as nota_avaliacao,
+    cast(
+      coalesce(
+        trim(
+          both '()'
+          from
+          df.num_avaliacoes
+        ),
+        '0'
+      ) as uinteger
+    ) as num_avaliacoes,
+    cast(df._pagina as utinyint) as _pagina,
+    cast(df._ordem as utinyint) as _ordem,
+    cast(df._data_coleta as date) as data_coleta,
+    cast(df._data_coleta as time) as horario_coleta,
+    coalesce(tratar_string(df.marca), 'GENERICA') as marca,
+    coalesce(tratar_string(df.produto), 'PRODUTO SEM NOME') as produto,
+    adicionar_preco_completo(
+      df.preco_velho_reais, df.preco_velho_centavos
+    ) as preco_velho,
+    adicionar_preco_completo(
+      df.preco_atual_reais, df.preco_atual_centavos
+    ) as preco_atual,
+    (df.preco_velho_reais is not null) as promocao
   from
     df
   where
@@ -66,45 +97,43 @@ with fase_1 as (
 )
 
 select distinct
+  f.produto,
+  f.preco_atual,
+  f.promocao,
+  cast(
     case
-      when f.marca = 'GENERICA'
-        then coalesce(
-          (
-            select
-              m.marca
-            from
-              marcas as m
-            where
-				      starts_with(f.produto, m.marca || ' ')
-              or contains(f.produto, ' ' || m.marca || ' ')
-				      or ends_with(f.produto, ' ' || m.marca)
-			      order by
-				      length(m.marca) desc
-			      limit
-				      1
-          )
-          , 'GENERICA'
-        )
-      else
-        f.marca
-    end as marca
-  , f.produto
-  , coalesce(f.preco_velho, f.preco_atual) as preco_velho
-  , f.preco_atual
-  , f.promocao
-  , cast(
-      case
-        when f.promocao = true
-          then ( (f.preco_velho - f.preco_atual) / f.preco_velho) * 100
-        else
-          0
-      end as decimal(4, 2)
-    ) as percentual_promocao
-  , f.nota_avaliacao
-  , f.num_avaliacoes
-  , f.data_coleta as _data_coleta
-  , f.horario_coleta as _horario_coleta
-  , f._pagina
-  , f._ordem
+      when
+        f.promocao = true
+        then ((f.preco_velho - f.preco_atual) / f.preco_velho) * 100
+      else 0
+    end as decimal(4, 2)
+  ) as percentual_promocao,
+  f.nota_avaliacao,
+  f.num_avaliacoes,
+  f.data_coleta as _data_coleta,
+  f.horario_coleta as _horario_coleta,
+  f._pagina,
+  f._ordem,
+  case
+    when f.marca = 'GENERICA'
+      then coalesce(
+        (
+          select m.marca
+          from
+            marcas as m
+          where
+            starts_with(f.produto, m.marca || ' ')
+            or contains(f.produto, ' ' || m.marca || ' ')
+            or ends_with(f.produto, ' ' || m.marca)
+          order by
+            length(m.marca) desc
+          limit
+            1
+        ),
+        'GENERICA'
+      )
+    else f.marca
+  end as marca,
+  coalesce(f.preco_velho, f.preco_atual) as preco_velho
 from
   fase_1 as f;
