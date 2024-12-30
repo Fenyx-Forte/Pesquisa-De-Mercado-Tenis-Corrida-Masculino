a7c98c69da9bd19ee35cfd0d32dbcd1bdb8098af
Fenyx Forte <fenyx.studies@gmail.com>
2024-12-30 17:55:32 -0300

    docs: documentando modulo

--- src/modulos/uteis/carregar_env.py
+++ src/modulos/uteis/carregar_env.py
@@ -1,5 +1,8 @@
+"""Módulo para carregar o arquivo de configuração ".env" do projeto."""
+
 from dotenv import load_dotenv
 
 
 def carregar_env() -> None:
+    """Carrega o arquivo de configuração ".env" do projeto."""
     load_dotenv("../.env")
