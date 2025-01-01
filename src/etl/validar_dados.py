de8a21432d315c5f5281aecbf4a3c2df32386c8e
Fenyx Forte <fenyx.studies@gmail.com>
2025-01-01 16:59:59 -0300

    docs: adicionando documentacao validar_dados.py

--- src/etl/validar_dados.py
+++ src/etl/validar_dados.py
@@ -1,3 +1,5 @@
+"""Módulo responsável por validar os dados de entrada e saida utilizando o contrato de dados."""
+
 import pandera.polars as pa
 import polars as pl
 from loguru import logger
@@ -5,25 +7,43 @@ from loguru import logger
 from modulos.contrato_de_dados import contrato_entrada, contrato_saida
 
 
-class ErroValidacao(Exception):
-    pass
+class ValidacaoError(Exception):
+    """Erro de validação de dados."""
 
 
 def validar_dados_entrada(df: pl.DataFrame) -> None:
+    """Valida os dados de entrada utilizando o contrato de dados.
+
+    Args:
+        df (pl.DataFrame): Dados de entrada.
+
+    Raises:
+        ValidacaoError: Se os dados não forem válidos.
+    """
     logger.info("Validando dados de entrada...")
     try:
         contrato_entrada.TenisCorridaEntrada.validate(df)
         logger.info("Dados validos")
     except pa.errors.SchemaError as exc:
         logger.error(exc)
-        raise ErroValidacao("Dados de entrada nao sao validos!")
+        mensagem_erro = "Dados de entrada não são válidos!"
+        raise ValidacaoError(mensagem_erro) from None
 
 
 def validar_dados_saida(df: pl.DataFrame) -> None:
+    """Valida os dados de saída utilizando o contrato de dados.
+
+    Args:
+        df (pl.DataFrame): Dados de entrada.
+
+    Raises:
+        ValidacaoError: Se os dados não forem válidos.
+    """
     logger.info("Validando dados de saida...")
     try:
         contrato_saida.TenisCorridaSaida.validate(df)
         logger.info("Dados validos")
     except pa.errors.SchemaError as exc:
         logger.error(exc)
-        raise ErroValidacao("Dados de saida nao sao validos!")
+        mensagem_erro = "Dados de saída não são válidos!"
+        raise ValidacaoError(mensagem_erro) from None
