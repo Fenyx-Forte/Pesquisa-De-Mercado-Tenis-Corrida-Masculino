824f01b79262ace0841b1b7f8bd11dfc6729d778
Fenyx Forte <fenyx.studies@gmail.com>
2025-01-01 16:12:10 -0300

    docs: adicionando documentacao extracao_sql.py

--- src/etl/extracao_sql.py
+++ src/etl/extracao_sql.py
@@ -1,14 +1,26 @@
+"""Módulo que extrai dados de um arquivo JSON usando DuckDB."""
+
 from duckdb import DuckDBPyConnection, DuckDBPyRelation
 from loguru import logger
 
 
 def extracao_json(
-    caminho_json: str, conexao: DuckDBPyConnection
+    caminho_json: str,
+    conexao: DuckDBPyConnection,
 ) -> DuckDBPyRelation:
+    """Extrai dados de um arquivo JSON.
+
+    Args:
+        caminho_json (str): Caminho para o arquivo JSON.
+        conexao (DuckDBPyConnection): Conexão com o banco de dados.
+
+    Returns:
+        DuckDBPyRelation: Dados extraídos.
+    """
     logger.info("Extraindo dados...")
 
-    df = conexao.read_json(caminho_json)
+    df_entrada = conexao.read_json(caminho_json)
 
     logger.info("Dados extraidos")
 
-    return df
+    return df_entrada
