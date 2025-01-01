bb519f350e2fc972d6bd6d19ea0cde183b3c8b3b
Fenyx Forte <fenyx.studies@gmail.com>
2025-01-01 16:57:25 -0300

    docs: adicionando documentacao salvar_sql.py

--- src/etl/salvar_sql.py
+++ src/etl/salvar_sql.py
@@ -1,3 +1,5 @@
+"""Módulo responsável por salvar os dados tratados, resultados do ETL, no banco de dados na nuvem."""
+
 from duckdb import DuckDBPyConnection, DuckDBPyRelation
 from loguru import logger
 
@@ -12,6 +14,17 @@ def salvar_dados(
     nome_arquivo: str,
     horario: str,
 ) -> None:
+    """Salva os dados tratados, resultados do ETL, no banco de dados na nuvem.
+
+    Além disso, salva o nome do arquivo e o horário em que os dados foram extraídos no banco de dados local.
+
+    Args:
+        query_insercao (str): Consulta SQL para inserir os dados no banco de dados na nuvem.
+        df (DuckDBPyRelation): Dados tratados, resultados do ETL.
+        conexao (DuckDBPyConnection): Conexão com o banco de dados na nuvem.
+        nome_arquivo (str): Nome do arquivo que será salvo no banco de dados local.
+        horario (str): Horário em que o arquivo será salvo no banco de dados local.
+    """
     logger.info("Iniciando conexao...")
     funcoes_sql.conexao_banco_de_dados_webscraping(conexao)
     logger.info("Conexao criada")
