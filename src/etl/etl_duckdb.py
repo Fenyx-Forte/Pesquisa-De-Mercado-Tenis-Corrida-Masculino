8a74425cb73980f537a95d57a1e96223994df6c9
Fenyx Forte <fenyx.studies@gmail.com>
2025-01-01 16:10:30 -0300

    docs: adicionando documentacao etl_duckdb.py

--- src/etl/etl_duckdb.py
+++ src/etl/etl_duckdb.py
@@ -1,3 +1,5 @@
+"""Pipeline para extração, processamento e salvamento de dados."""
+
 from duckdb import connect
 from loguru import logger
 
@@ -11,6 +13,7 @@ from modulos.uteis import funcoes_sql, meu_tempo, minhas_queries
 
 
 def pipeline() -> None:
+    """Pipeline para extração, processamento e salvamento de dados."""
     # Extracao
     horario = meu_tempo.data_agora_string()
 
@@ -22,22 +25,22 @@ def pipeline() -> None:
 
     # Verificacao
     arquivo_ja_foi_salvo = duckdb_local.verifica_se_arquivo_ja_foi_salvo(
-        nome_arquivo
+        nome_arquivo,
     )
 
     if arquivo_ja_foi_salvo:
         logger.info("Arquivo ja foi salvo no banco de dados!")
-        return None
+        return
 
     conexao = connect(":memory:")
 
-    df = extracao_sql.extracao_json(caminho_json, conexao)
+    df_entrada = extracao_sql.extracao_json(caminho_json, conexao)
 
     # Validacao
     query_entrada = minhas_queries.cast_polars_entrada()
 
     validar_dados.validar_dados_entrada(
-        funcoes_sql.query_duckbdb_para_pl(query_entrada, df, conexao)
+        funcoes_sql.query_duckbdb_para_pl(query_entrada, df_entrada, conexao),
     )
 
     # Transformacao
@@ -45,20 +48,30 @@ def pipeline() -> None:
 
     query_transfomacao = funcoes_sql.ler_conteudo_query(caminho_query)
 
-    df = funcoes_sql.query_duckdb_para_duckdb(query_transfomacao, df, conexao)
+    df_saida = funcoes_sql.query_duckdb_para_duckdb(
+        query_transfomacao,
+        df_entrada,
+        conexao,
+    )
 
     # Validacao
     query_saida = minhas_queries.cast_polars_saida()
 
     validar_dados.validar_dados_saida(
-        funcoes_sql.query_duckbdb_para_pl(query_saida, df, conexao)
+        funcoes_sql.query_duckbdb_para_pl(query_saida, df_saida, conexao),
     )
 
     # Salvar
     query_insercao = funcoes_sql.ler_conteudo_query(
-        "../sql/dml/inserir_dados.sql"
+        "../sql/dml/inserir_dados.sql",
     )
 
-    salvar_sql.salvar_dados(query_insercao, df, conexao, nome_arquivo, horario)
+    salvar_sql.salvar_dados(
+        query_insercao,
+        df_saida,
+        conexao,
+        nome_arquivo,
+        horario,
+    )
 
     conexao.close()
