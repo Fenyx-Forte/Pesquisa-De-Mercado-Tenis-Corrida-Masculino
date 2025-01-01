b7b07a430484f432145c7cb741f3c679aa803ad4
Fenyx Forte <fenyx.studies@gmail.com>
2025-01-01 15:59:42 -0300

    docs: melhorando documentacao

--- src/modulos/uteis/configuracao_polars.py
+++ src/modulos/uteis/configuracao_polars.py
@@ -4,5 +4,9 @@ import polars as pl
 
 
 def configuracao_polars(caminho_arquivo: str) -> None:
-    """Carrega a configuração do polars."""
+    """Carrega a configuração do polars.
+
+    Args:
+        caminho_arquivo (str): Caminho para o arquivo de configuração do polars.
+    """
     pl.Config.load_from_file(caminho_arquivo)
