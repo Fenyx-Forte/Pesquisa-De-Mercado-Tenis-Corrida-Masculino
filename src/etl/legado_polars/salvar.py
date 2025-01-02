d2b97bfdaa8acb96dcbec53829a65638fae3e1c9
Fenyx Forte <fenyx.studies@gmail.com>
2025-01-02 08:01:25 -0300

    docs: adicionando documentacao salvar.py

--- src/etl/legado_polars/salvar.py
+++ src/etl/legado_polars/salvar.py
@@ -1,8 +1,16 @@
+"""Módulo que salva os dados tratados do Webscraping em um arquivo parquet."""
+
 import polars as pl
 from loguru import logger
 
 
 def salvar_dados_parquet(caminho_arquivo: str, df: pl.DataFrame) -> None:
+    """Salva os dados tratados do Webscraping em um arquivo parquet.
+
+    Args:
+        caminho_arquivo (str): Caminho para o arquivo parquet.
+        df (pl.DataFrame): Dados tratados.
+    """
     logger.info("Salvando dados em parquet...")
     df.write_parquet(caminho_arquivo)
     logger.info("Dados salvos!")
