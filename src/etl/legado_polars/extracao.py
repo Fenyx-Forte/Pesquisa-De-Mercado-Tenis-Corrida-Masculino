6e76d8d3a3808a583c0a85932adb8a271a0ac635
Fenyx Forte <fenyx.studies@gmail.com>
2025-01-02 07:49:29 -0300

    docs: adicionando documentacao extracao.py

--- src/etl/legado_polars/extracao.py
+++ src/etl/legado_polars/extracao.py
@@ -1,7 +1,17 @@
+"""Módulo que extrai os dados do webscraping a partir de um arquivo json."""
+
 import polars as pl
 from loguru import logger
 
 
 def extrair_dados_json(caminho_arquivo: str) -> pl.DataFrame:
+    """Extrai os dados do arquivo json e retorna um dataframe polars.
+
+    Args:
+        caminho_arquivo (str): Caminho para o arquivo json.
+
+    Returns:
+        pl.DataFrame: Dados extraidos do arquivo json.
+    """
     logger.info("Extraindo dados json...")
     return pl.read_json(caminho_arquivo)
