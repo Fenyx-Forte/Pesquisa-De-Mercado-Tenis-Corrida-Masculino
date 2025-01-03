aef2165aa643e222af7990b494511bf872af79bd
Fenyx Forte <fenyx.studies@gmail.com>
2025-01-03 08:56:02 -0300

    docs: adicionando documentacao dashboard/duckdb_local.py

--- src/dashboard/uteis/duckdb_local.py
+++ src/dashboard/uteis/duckdb_local.py
@@ -1,12 +1,20 @@
+"""Módulo usado para criar a conexão com o duckdb local, que contém os dados usados no dashboard."""
+
 from duckdb import DuckDBPyConnection, connect
 
 
 def conexao_duckdb_local() -> DuckDBPyConnection:
+    """Conecta a um banco de dados DuckDB local.
+
+    Args:
+        caminho (str): Caminho do arquivo do banco de dados.
+
+    Returns:
+        DuckDBPyConnection: Conexão ao banco de dados DuckDB.
+    """
     caminho = "../dados/duckdb_database.db"
 
-    conexao = connect(
+    return connect(
         database=caminho,
         read_only=True,
     )
-
-    return conexao
