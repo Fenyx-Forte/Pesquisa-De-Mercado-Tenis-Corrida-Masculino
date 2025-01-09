0628ad45395299e295a390ca943c05fef1cc2001
Fenyx Forte <fenyx.studies@gmail.com>
2025-01-09 14:59:26 -0300

    docs: melhorando documentacao pagina preco medio

--- src/dashboard/paginas/pagina_preco_medio.py
+++ src/dashboard/paginas/pagina_preco_medio.py
@@ -1,3 +1,5 @@
+"""Página Preço Médio."""
+
 from dash import (
     Input,
     Output,
@@ -15,7 +17,6 @@ from dash_bootstrap_components import (
 )
 
 from dashboard.processamento import gerenciador
-from dashboard.processamento.paginas import processamento_preco_medio
 from dashboard.uteis import (
     componentes_pagina,
     formatacoes,
@@ -33,14 +34,32 @@ register_page(
 
 
 def id_pagina() -> str:
+    """Id da página.
+
+    Returns:
+        str: id da página.
+    """
     return "preco_medio"
 
 
 def titulo_pagina() -> str:
+    """Título da página.
+
+    Returns:
+        str: Título da página.
+    """
     return "Preço Médio"
 
 
 def definicoes_colunas_tabela(sufixo_coluna: str) -> list[dict]:
+    """Retorna uma lista de dicionários que representam as definições das colunas da tabela.
+
+    Args:
+        sufixo_coluna (str): Sufixo que define qual configuração será usada.
+
+    Returns:
+        list[dict]: Configuração a ser usada pela tabela a depender de sufixo_coluna.
+    """
     configuracao_marca = {
         "headerName": "Marca",
         "field": "marca",
@@ -88,15 +107,21 @@ def definicoes_colunas_tabela(sufixo_coluna: str) -> list[dict]:
 
 
 def informacao(tabela: AgGrid) -> html.Div:
-    conteudo = html.Div(
+    """Retorna uma Div contendo a tabela.
+
+    Args:
+        tabela (AgGrid): Tabela com as informações.
+
+    Returns:
+        html.Div: Div contendo a tabela.
+    """
+    return html.Div(
         html.Div(
             tabela,
             className="tabela",
         ),
         className="informacao",
     )
-
-    return conteudo
 
 
 def coluna(
@@ -107,6 +132,19 @@ def coluna(
     dados_entre_200_e_400: list[dict],
     dados_acima_de_400: list[dict],
 ) -> html.Div:
+    """Retorna uma das 3 colunas usadas no corpo da página.
+
+    Args:
+        titulo_cabecalho (str): Título do cabeçalho da coluna.
+        periodo (str): Período utilizado para filtrar os dados.
+        sufixo_coluna (str): Sufixo que será adicionado ao nome das colunas para criar as definições das colunas.
+        dados_abaixo_de_200 (list[dict]): Dados com valor abaixo de 200.
+        dados_entre_200_e_400 (list[dict]): Dados com valor entre 200 e 400.
+        dados_acima_de_400 (list[dict]): Dados com valor acima de 400.
+
+    Returns:
+        html.Div: Coluna que será usada no corpo da página.
+    """
     tabela_abaixo_de_200 = componentes_pagina.tabela_ag_grid(
         dados=dados_abaixo_de_200,
         id_completo=f"{id_pagina()}_tabela_{sufixo_coluna}_200",
@@ -125,7 +163,7 @@ def coluna(
         definicoes_colunas=definicoes_colunas_tabela(sufixo_coluna),
     )
 
-    conteudo = html.Div(
+    return html.Div(
         [
             componentes_pagina.cabecalho_coluna(
                 id_pagina=id_pagina(),
@@ -148,10 +186,13 @@ def coluna(
         ],
     )
 
-    return conteudo
-
 
 def colunas() -> Row:
+    """Retorna um conjunto de colunas.
+
+    Returns:
+        Row: Linha que contém todas as colunas do corpo da página.
+    """
     periodo_hoje = gerenciador.retorna_periodo_hoje()
 
     periodo_escolhido = gerenciador.retorna_periodo_ultima_semana()
@@ -164,7 +205,7 @@ def colunas() -> Row:
 
     dados_historico = gerenciador.pagina_preco_medio_dados_historico()
 
-    conteudo = Row(
+    return Row(
         [
             Col(
                 coluna(
@@ -205,8 +246,6 @@ def colunas() -> Row:
         ],
         class_name="linha_colunas",
     )
-
-    return conteudo
 
 
 layout = html.Div(
@@ -259,7 +298,21 @@ clientside_callback(
     prevent_initial_call=True,
     running=[(Output(f"{id_pagina()}_botao", "disabled"), True, False)],
 )
-def preco_medio_atualiza_dados_periodo_escolhido(titulo, data_inicio, data_fim):
+def preco_medio_atualiza_dados_periodo_escolhido(
+    titulo: str,
+    data_inicio: str,
+    data_fim: str,
+) -> list[dict | None | str]:
+    """Retorna os dados atualizados da coluna "escolhido" para um período especificado.
+
+    Args:
+        titulo (str): Título atual do modal_erro. Se não for igual a "", então houve algum erro detectado pelo modal_erro.
+        data_inicio (str): Data de início do período a ser considerado.
+        data_fim (str): Data de fim do período a ser considerado.
+
+    Returns:
+        list[dict | None | str]: Lista com os dados necessários para atualizar a coluna.
+    """
     if titulo != "":
         raise PreventUpdate
 
@@ -269,17 +322,18 @@ def preco_medio_atualiza_dados_periodo_escolhido(titulo, data_inicio, data_fim):
     periodo_novo = uteis_processamento.retorna_periodo(data_inicio, data_fim)
 
     dados_escolhido = gerenciador.pagina_preco_medio_atualiza_dados_escolhido(
-        data_inicio, data_fim
+        data_inicio,
+        data_fim,
     )
 
-    return (
+    return [
         dados_escolhido["dados_abaixo_de_200"],
         dados_escolhido["dados_entre_200_e_400"],
         dados_escolhido["dados_acima_de_400"],
         None,
         None,
         periodo_novo,
-    )
+    ]
 
 
 clientside_callback(
